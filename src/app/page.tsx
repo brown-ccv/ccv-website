9d69729314f712910568090905557f08a8e90e2f
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-02 13:56:03 -0400

    chore: appease the linter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
-import Image from "next/image";
-import Calendar from "@/components/Calendar";
+import Calendar from "@/components/calendar/Calendar";
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-        <Calendar/>
+      <Calendar />
     </main>
   );
 }
