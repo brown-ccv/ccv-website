d83780c489c860766442f355896bf2331eb43fe8
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-02 13:33:52 -0400

    feat: CalendarEvent component

---
+++ src/components/calendar/CalendarEvent.tsx
@@ -0,0 +1,41 @@
+import React from "react";
+import { DataProps } from "@/components/calendar/Calendar";
+
+const CalendarEvent: React.FC<DataProps> = ({
+  date_time,
+  title,
+  url,
+  date,
+  date_utc,
+  description_long,
+}) => {
+  const descriptionLong = description_long.replace(/<(.|\n)*?>/g, "");
+  const dateTime = new Date(date_utc.replace(/-/g, "/"));
+  const normalDate =
+    new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(dateTime) +
+    ", " +
+    date;
+  return (
+    <div className="bg-gray flex gap-6 overflow-hidden column item">
+      <div className="event">
+        <div className="text-neutral-900 text-sm">{normalDate}</div>
+        <div>
+          <a
+            className="big-font font-bold text-secondary-blue-500 no-underline leading-4"
+            href={url}
+            target="_blank"
+          >
+            {" "}
+            {title}{" "}
+          </a>
+        </div>
+        <div className="datetime text-xs">{date_time}</div>
+        <div className="py-2">
+          {description_long && <p>{descriptionLong}</p>}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CalendarEvent;
