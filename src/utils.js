396f37e539dce33e390127359bd92f9b1aeffd12
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-06 14:38:40 -0400

    lint: appease the linter

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,3 @@
-
 //
 // Calendar Utils
 //
@@ -11,10 +10,10 @@
  * @returns {number} The number of weeks in the month.
  */
 export const weeksInMonth = (month, year) => {
-    // month = # (0 indexed), year = #
-    const firstDay = new Date(year, month, 1).getDay(); // day of the week of 1st
-    const daysInMonth = new Date(year, month + 1, 0).getDate();
-    return Math.ceil((firstDay + daysInMonth) / 7);
+  // month = # (0 indexed), year = #
+  const firstDay = new Date(year, month, 1).getDay() // day of the week of 1st
+  const daysInMonth = new Date(year, month + 1, 0).getDate()
+  return Math.ceil((firstDay + daysInMonth) / 7)
 }
 
 /**
@@ -26,44 +25,44 @@ export const weeksInMonth = (month, year) => {
  * @returns {string} The date as a string.
  */
 export const getStringDate = (month, day, year) => {
-    const mm = month < 10 ? '0' + month : month;
-    const dd = day < 10 ? '0' + day : day;
-    return year + '-' + mm + '-' + dd;
+  const mm = month < 10 ? "0" + month : month
+  const dd = day < 10 ? "0" + day : day
+  return year + "-" + mm + "-" + dd
 }
 
 export const ALL_MONTHS = [
-    'January',
-    'February',
-    'March',
-    'April',
-    'May',
-    'June',
-    'July',
-    'August',
-    'September',
-    'October',
-    'November',
-    'December',
-];
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+]
 
 export const ALL_DAYS_OF_WEEK = [
-    'Sun',
-    'Mon',
-    'Tues',
-    'Weds',
-    'Thurs',
-    'Fri',
-    'Sat',
-];
+  "Sun",
+  "Mon",
+  "Tues",
+  "Weds",
+  "Thurs",
+  "Fri",
+  "Sat",
+]
 
-export const DISPLAY_YEAR = new Date().getFullYear();
+export const DISPLAY_YEAR = new Date().getFullYear()
 
-export const DISPLAY_MONTH = new Date().getMonth() + 1;
+export const DISPLAY_MONTH = new Date().getMonth() + 1
 
-export const DISPLAY_DAY = new Date().getDate();
+export const DISPLAY_DAY = new Date().getDate()
 
 export const TODAYS_DATE = getStringDate(
-    new Date().getMonth() + 1,
-    new Date().getDate(),
-    new Date().getFullYear()
-);
+  new Date().getMonth() + 1,
+  new Date().getDate(),
+  new Date().getFullYear()
+)
