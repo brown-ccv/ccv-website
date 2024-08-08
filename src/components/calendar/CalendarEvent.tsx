6b3dfa047b4f133cb212531de30a4afdc13d091f
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-08 15:27:34 -0400

    refactor: pass full event data to CalEvent

--- src/components/calendar/CalendarEvent.tsx
+++ src/components/calendar/CalendarEvent.tsx
@@ -1,20 +1,17 @@
 import React from "react";
 import { DataProps } from "@/components/calendar/Calendar";
 
-const CalendarEvent: React.FC<DataProps> = ({
-  date_time,
-  title,
-  url,
-  date,
-  date_utc,
-  description_long,
-}) => {
-  const descriptionLong = description_long.replace(/<(.|\n)*?>/g, "");
-  const dateTime = new Date(date_utc.replace(/-/g, "/"));
+interface EventProps {
+  data: DataProps;
+}
+
+const CalendarEvent: React.FC<EventProps> = ({ data }) => {
+  const descriptionLong = data.description_long.replace(/<(.|\n)*?>/g, "");
+  const dateTime = new Date(data.date_utc.replace(/-/g, "/"));
   const normalDate =
     new Intl.DateTimeFormat("en-US", { weekday: "long" }).format(dateTime) +
     ", " +
-    date;
+    data.date;
   return (
     <div className="bg-gray flex gap-6 overflow-hidden column min-w-[30ch]">
       <div className="event">
@@ -22,16 +19,16 @@ const CalendarEvent: React.FC<DataProps> = ({
         <div>
           <a
             className="text-xl font-bold text-secondary-blue-500 no-underline leading-4"
-            href={url}
+            href={data.url}
             target="_blank"
           >
             {" "}
-            {title}{" "}
+            {data.title}{" "}
           </a>
         </div>
-        <div className="text-primary-500 text-xs">{date_time}</div>
+        <div className="text-primary-500 text-xs">{data.date_time}</div>
         <div className="py-2">
-          {description_long && <p>{descriptionLong}</p>}
+          {data.description_long && <p>{descriptionLong}</p>}
         </div>
       </div>
     </div>
