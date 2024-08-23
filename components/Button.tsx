f6620dd0f13589b14da69cd90c84652e76a7c078
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-08-23 12:02:59 -0400

    chore: create variant property instead of exporting multiple buttons

--- components/Button.tsx
+++ components/Button.tsx
@@ -1,47 +1,45 @@
-import React, { type ReactNode } from "react";
+import React, { type ReactNode } from "react"
 
 interface ButtonProps
   extends React.PropsWithChildren<
     React.ButtonHTMLAttributes<HTMLButtonElement>
   > {
-  icon?: ReactNode;
+  icon?: ReactNode
+  variant?: string
 }
 
-const buttonStyles =
-  "flex items-center gap-2 px-3 py-2 w-max rounded transition-all hover:shadow-md";
+export default function Button({
+  icon,
+  variant = "primary",
+  children,
+  ...delegated
+}: ButtonProps) {
+  let buttonStyles
 
-export default function Button({ icon, children, ...delegated }: ButtonProps) {
+  switch (variant) {
+    case "primary":
+      buttonStyles =
+        "bg-secondary-blue-500 text-white hover:bg-secondary-blue-700"
+      break
+    case "secondary":
+      buttonStyles = "bg-secondary-yellow-500 hover:bg-secondary-yellow-700"
+      break
+    case "tertiary":
+      buttonStyles =
+        "bg-neutral-50 text-secondary-blue-700 hover:bg-neutral-100"
+      break
+    default:
+      buttonStyles =
+        "bg-secondary-blue-500 text-white hover:bg-secondary-blue-700"
+  }
+
   return (
     <button
-      className={`${buttonStyles} bg-secondary-blue-500 text-white hover:bg-secondary-blue-700`}
+      className={`${buttonStyles} flex items-center gap-2 px-3 py-2 w-max rounded transition-all hover:shadow-md`}
       {...delegated}
     >
       {icon}
       <span>{children}</span>
     </button>
-  );
-}
-
-export function ButtonSecondary({ icon, children, ...delegated }: ButtonProps) {
-  return (
-    <button
-      className={`${buttonStyles} bg-secondary-yellow-500 hover:bg-secondary-yellow-700`}
-      {...delegated}
-    >
-      {icon}
-      <span>{children}</span>
-    </button>
-  );
-}
-
-export function ButtonTertiary({ icon, children, ...delegated }: ButtonProps) {
-  return (
-    <button
-      className={`${buttonStyles} bg-neutral-50 text-secondary-blue-700 hover:bg-neutral-100`}
-      {...delegated}
-    >
-      {icon}
-      <span>{children}</span>
-    </button>
-  );
+  )
 }
