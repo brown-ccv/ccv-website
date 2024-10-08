fe581d914c2b722c43e7375df18699aad5d2091d
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-08 15:15:48 -0400

    feat: ccv logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,12 @@
 import Calendar from "@/components/calendar/Calendar"
 import LogoBrown from "@/components/assets/LogoBrown"
+import LogoCcv from "@/components/assets/LogoCcv"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <LogoBrown />
+      <LogoCcv />
     </main>
   )
 }
