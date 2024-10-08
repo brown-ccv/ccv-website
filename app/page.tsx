1e3bd71428513bd19727bd6076b49389a87dec9f
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-08 14:25:55 -0400

    feat: Brown/OIT logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,10 @@
 import Calendar from "@/components/calendar/Calendar"
+import LogoBrown from "@/components/assets/LogoBrown"
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Calendar />
+      <LogoBrown />
     </main>
   )
 }
