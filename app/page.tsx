90f4cfd6b80700a6d3397d87e0656d07ebbdc58b
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-10 14:04:11 -0400

    refactor: use logos from main

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,6 @@ import LogoCcv from "@/components/assets/LogoCcv"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LogoBrown />
-      <LogoCcv />
-    </main>
+    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
   )
 }
