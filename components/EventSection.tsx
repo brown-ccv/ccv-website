724b9da647a78fb014f79dd691d959f571fb3327
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-15 16:35:41 -0400

    refactor: map over views

--- components/EventSection.tsx
+++ components/EventSection.tsx
@@ -23,13 +23,15 @@ const EventSection = () => {
   const [dataFuture, setDataFuture] = useState([])
   const [dataPast, setDataPast] = useState([])
   const [isLoading, setLoading] = useState(true)
-  const [view, setView] = useState("upcoming")
+  const [view, setView] = useState("Upcoming")
   const currentDate = new Date()
   const today = getStringDate(
     currentDate.getMonth() + 1,
     currentDate.getDate(),
     currentDate.getFullYear()
   )
+
+  const CAL_VIEW_ARRAY = ["Weekly", "Monthly", "Upcoming"]
 
   useEffect(() => {
     fetch(
@@ -40,7 +42,6 @@ const EventSection = () => {
       .then((res) => res.json())
       .then((data) => {
         setDataFuture(data)
-        console.log(data)
         setLoading(false)
       })
     fetch(
@@ -51,7 +52,6 @@ const EventSection = () => {
       .then((res) => res.json())
       .then((data) => {
         setDataPast(data)
-        console.log(data)
       })
   }, [])
   return (
@@ -64,32 +64,24 @@ const EventSection = () => {
       <section>
         <div className="hidden min-h-8 relative lg:block">
           <div className="toggle-btn">
-            <p
-              id="week"
-              className={view === "weekly" ? "selected" : ""}
-              onClick={() => setView("weekly")}
-            >
-              Weekly
-            </p>
-            <p
-              id="month"
-              className={view === "monthly" ? "selected" : ""}
-              onClick={() => setView("monthly")}
-            >
-              Monthly
-            </p>
-            <p
-              id="upcoming"
-              className={view === "upcoming" ? "selected" : ""}
-              onClick={() => setView("upcoming")}
-            >
-              Upcoming
-            </p>
+            {CAL_VIEW_ARRAY.map((item) => {
+              return (
+                <p
+                  id={item}
+                  key={item}
+                  className={view === item ? "selected" : ""}
+                  role="button"
+                  onClick={() => setView(item)}
+                >
+                  {item}
+                </p>
+              )
+            })}
           </div>
         </div>
         {isLoading && <p>Loading...</p>}
         {!dataFuture && <p>No event data</p>}
-        {view === "upcoming" && (
+        {view === "Upcoming" && (
           <div className="flex flex-wrap justify-between gap-4">
             {dataFuture &&
               dataFuture.slice(0, 4).map((e: DataProps, i) => {
@@ -116,7 +108,7 @@ const EventSection = () => {
               })}
           </div>
         )}
-        {view === "weekly" && (
+        {view === "Weekly" && (
           <div className="h-0 min-h-[768px] ">
             <CalendarWeekly
               today={today}
