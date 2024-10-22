9b59a9bb204f9f852959eaaee6563f0ec3a77654
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-22 14:21:51 -0400

    style: searchbar columns when it is too crowded

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -17,10 +17,10 @@ import Link from "next/link"
 const Navbar = () => {
   return (
     <Disclosure as="nav" className="bg-neutral-50 shadow">
-      <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
-        <div className="flex h-16 justify-between">
-          <div className="flex px-2 lg:px-0">
-            <div className="hidden md:flex flex-shrink-0 items-center gap-3">
+      <div className="mx-auto max-w-7xl p-2 sm:px-4 lg:px-8">
+        <div className="flex flex-col gap-3.5 xl:flex-row xl:h-16 xl:justify-between">
+          <div className="flex justify-between px-2 lg:px-0">
+            <div className="flex flex-shrink-0 items-center gap-3">
               <a href="https://it.brown.edu/" target="_blank">
                 <span className="sr-only">Brown IT Website</span>
                 <LogoBrown width={115} />
@@ -33,8 +33,23 @@ const Navbar = () => {
             <div className="hidden lg:ml-6 lg:flex lg:items-center lg:space-x-8">
               <NavLinks />
             </div>
+            <div className="flex items-center lg:hidden">
+              {/* Mobile menu button */}
+              <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+                <span className="absolute -inset-0.5" />
+                <span className="sr-only">Open main menu</span>
+                <Bars3Icon
+                  aria-hidden="true"
+                  className="block h-6 w-6 group-data-[open]:hidden"
+                />
+                <XMarkIcon
+                  aria-hidden="true"
+                  className="hidden h-6 w-6 group-data-[open]:block"
+                />
+              </DisclosureButton>
+            </div>
           </div>
-          <div className="flex flex-1 items-center justify-center px-2 gap-10 lg:ml-6 lg:justify-end">
+          <div className="flex flex-1 items-center justify-center px-2 gap-10 lg:ml-6">
             <div>
               <a
                 href="https://docs.ccv.brown.edu"
@@ -49,6 +64,7 @@ const Navbar = () => {
                 />
               </a>
             </div>
+
             <div className="w-full max-w-lg lg:max-w-xs">
               <label htmlFor="search" className="sr-only">
                 Search
@@ -69,21 +85,6 @@ const Navbar = () => {
                 />
               </div>
             </div>
-          </div>
-          <div className="flex items-center lg:hidden">
-            {/* Mobile menu button */}
-            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
-              <span className="absolute -inset-0.5" />
-              <span className="sr-only">Open main menu</span>
-              <Bars3Icon
-                aria-hidden="true"
-                className="block h-6 w-6 group-data-[open]:hidden"
-              />
-              <XMarkIcon
-                aria-hidden="true"
-                className="hidden h-6 w-6 group-data-[open]:block"
-              />
-            </DisclosureButton>
           </div>
         </div>
       </div>
