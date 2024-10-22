5923c363f63ab6ac6274177bf16c643db0c59b41
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-22 12:43:01 -0400

    feat: tailwindui styled navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,74 +2,98 @@ import LogoCcv from "@/components/assets/LogoCcv"
 import LogoBrown from "@/components/assets/LogoBrown"
 import NavLinks from "@/components/NavLinks"
 
+import {
+  Disclosure,
+  DisclosureButton,
+  DisclosurePanel,
+} from "@headlessui/react"
+import {
+  MagnifyingGlassIcon,
+  DocumentTextIcon,
+} from "@heroicons/react/20/solid"
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
+import Link from "next/link"
+
 const Navbar = () => {
   return (
-    <nav className="bg-neutral-50 h-14 min-h-14">
-      <div className="max-w-7xl px-2 sm:px-6 lg:px-8">
-        <div className="relative flex h-16 items-center justify-between">
-          <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
-            <button
-              type="button"
-              className="relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
-              aria-controls="mobile-menu"
-              aria-expanded="false"
-            >
-              <span className="absolute -inset-0.5"></span>
-              <span className="sr-only">Open main menu</span>
-              <svg
-                className="block h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
-                />
-              </svg>
-              <svg
-                className="hidden h-6 w-6"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke-width="1.5"
-                stroke="currentColor"
-                aria-hidden="true"
-              >
-                <path
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  d="M6 18L18 6M6 6l12 12"
-                />
-              </svg>
-            </button>
-          </div>
-
-          <div className="flex flex-1 items-center justify-center sm:items-stretch sm:justify-start flex-shrink-0 flex-grow">
-            <div className="hidden sm:flex flex-shrink-0 items-center gap-3.5">
-              <div>
-                <LogoBrown width={105} />
-              </div>
-              <div>
+    <Disclosure as="nav" className="bg-neutral-50 shadow">
+      <div className="mx-auto max-w-7xl px-2 sm:px-4 lg:px-8">
+        <div className="flex h-16 justify-between">
+          <div className="flex px-2 lg:px-0">
+            <div className="hidden md:flex flex-shrink-0 items-center gap-3">
+              <a href="https://it.brown.edu/" target="_blank">
+                <span className="sr-only">Brown IT Website</span>
+                <LogoBrown width={115} />
+              </a>
+              <Link href="/">
+                <span className="sr-only">Home</span>
                 <LogoCcv width={105} />
-              </div>
+              </Link>
             </div>
-            <div className="hidden sm:ml-6 sm:block">
-              <div className="flex space-x-4">
-                <NavLinks />
+            <div className="hidden lg:ml-6 lg:flex lg:items-center lg:space-x-8">
+              <NavLinks />
+            </div>
+          </div>
+          <div className="flex flex-1 items-center justify-center px-2 gap-10 lg:ml-6 lg:justify-end">
+            <div>
+              <a
+                href="https://docs.ccv.brown.edu"
+                target="_blank"
+                aria-label="CCV Services' Documentation"
+                className="bg-white text-secondary-blue-700 inline-flex items-center rounded-md px-3 py-2 "
+              >
+                Docs
+                <DocumentTextIcon
+                  aria-hidden="true"
+                  className="h-5 w-5 text-secondary-blue-700"
+                />
+              </a>
+            </div>
+            <div className="w-full max-w-lg lg:max-w-xs">
+              <label htmlFor="search" className="sr-only">
+                Search
+              </label>
+              <div className="relative">
+                <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
+                  <MagnifyingGlassIcon
+                    aria-hidden="true"
+                    className="h-5 w-5 text-gray-400"
+                  />
+                </div>
+                <input
+                  id="search"
+                  name="search"
+                  type="search"
+                  placeholder="Search"
+                  className="block w-full rounded-md border-0 bg-white py-1.5 pl-10 pr-3 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
               </div>
             </div>
           </div>
+          <div className="flex items-center lg:hidden">
+            {/* Mobile menu button */}
+            <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-gray-400 hover:bg-gray-100 hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+              <span className="absolute -inset-0.5" />
+              <span className="sr-only">Open main menu</span>
+              <Bars3Icon
+                aria-hidden="true"
+                className="block h-6 w-6 group-data-[open]:hidden"
+              />
+              <XMarkIcon
+                aria-hidden="true"
+                className="hidden h-6 w-6 group-data-[open]:block"
+              />
+            </DisclosureButton>
+          </div>
         </div>
       </div>
-      <div className="hidden" id="mobile-menu">
-        <div className="space-y-1 px-2 pb-3 pt-2">
-          <NavLinks />
+
+      <DisclosurePanel className="lg:hidden">
+        <div className="space-y-1 pb-3 pt-2">
+          <NavLinks linkType="mobile" />
         </div>
-      </div>
-    </nav>
+      </DisclosurePanel>
+    </Disclosure>
   )
 }
 export default Navbar
