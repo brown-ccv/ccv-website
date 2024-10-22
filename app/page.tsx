570aaaff0eb0ce20842d023e7bea306c38663997
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-10-22 16:41:04 -0400

    chore: update main page with footer

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,9 @@
-import Calendar from "@/components/calendar/Calendar"
-import LogoBrown from "@/components/assets/LogoBrown"
-import LogoCcv from "@/components/assets/LogoCcv"
+import Footer from "@/components/Footer"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <LogoBrown />
-      <LogoCcv />
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <Footer />
     </main>
   )
 }
