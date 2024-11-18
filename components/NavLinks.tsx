701eeef8d537ecd84b4253c5333ce67b5f9aeb9d
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-11-18 14:43:12 -0500

    feat: use shadcn for navbar

--- components/NavLinks.tsx
+++ components/NavLinks.tsx
@@ -1,59 +1,64 @@
-"use client"
 import Link from "next/link"
 import { usePathname } from "next/navigation"
-import { Menu, MenuButton, MenuItem, MenuItems } from "@headlessui/react"
-import React from "react"
-import { ChevronDownIcon, XMarkIcon } from "@heroicons/react/24/outline"
+import { Menu, MenuItem, MenuItems } from "@headlessui/react"
+import MenuDropdown from "@/components/MenuDropdown"
+import React, { useRef } from "react"
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
 
-interface NavLinksProps {
-  linkType?: string
-}
-
-const NavLinks: React.FC<NavLinksProps> = ({ linkType }) => {
+const NavLinks: React.FC = () => {
   const navigation = [
     {
       title: "Services",
       href: "/services",
       routes: [
-        { name: "Classroom", href: "/services/classroom" },
-        { name: "Computing", href: "/services/computing" },
+        { title: "Classroom", href: "/services/classroom" },
+        { title: "Computing", href: "/services/computing" },
         {
-          name: "Campus File Storage and Transfer",
+          title: "Campus File Storage and Transfer",
           href: "/services/file-storage-and-transfer",
         },
-        { name: "Rates", href: "/services/rates" },
-        { name: "Visualization Systems", href: "/services/visualization" },
-        { name: "Consulting", href: "/services/consulting" },
+        { title: "Rates", href: "/services/rates" },
+        { title: "Visualization Systems", href: "/services/visualization" },
+        { title: "Consulting", href: "/services/consulting" },
       ],
     },
     {
       title: "Documentation",
       href: "/documentation",
       routes: [
-        { name: "Oscar", href: "https://docs.ccv.brown.edu" },
+        { title: "Oscar", href: "https://docs.ccv.brown.edu" },
         {
-          name: "Stronghold",
+          title: "Stronghold",
           href: "/services/computing#data-risk-level-3-computing-(stronghold)",
         },
-        { name: "Globus", href: "/services/file-storage-and-transfer#globus" },
-        { name: "Jupyter Hub", href: "/services/classroom#jupyterhub" },
+        { title: "Globus", href: "/services/file-storage-and-transfer#globus" },
+        { title: "Jupyter Hub", href: "/services/classroom#jupyterhub" },
       ],
     },
     {
       title: "Our Work",
       href: "/our-work",
       routes: [
-        { name: "Collaborations", href: "#" },
+        { title: "Collaborations", href: "#" },
         {
-          name: "Software",
+          title: "Software",
           href: "/our-work/software",
         },
         {
-          name: "Workshops and Talks",
+          title: "Workshops and Talks",
           href: "/our-work/workshops-and-talks",
         },
         {
-          name: "Publications",
+          title: "Publications",
           href: "https://publications.ccv.brown.edu",
         },
       ],
@@ -62,17 +67,17 @@ const NavLinks: React.FC<NavLinksProps> = ({ linkType }) => {
       title: "Help",
       href: "/help",
       routes: [
-        { name: "Submit a Ticket", href: "mailto:support@ccv.brown.edu" },
+        { title: "Submit a Ticket", href: "mailto:support@ccv.brown.edu" },
         {
-          name: "Discourse",
+          title: "Discourse",
           href: "https://ask.cyberinfrastructure.org/c/brown-research-computing/37",
         },
         {
-          name: "Slack",
+          title: "Slack",
           href: "https://ccv-share.slack.com/signup#/",
         },
         {
-          name: "Office Hours",
+          title: "Office Hours",
           href: "https://events.brown.edu/ccv/month",
         },
       ],
@@ -81,33 +86,33 @@ const NavLinks: React.FC<NavLinksProps> = ({ linkType }) => {
       title: "About",
       href: "/about",
       routes: [
-        { name: "Mission", href: "/about#mission" },
+        { title: "Mission", href: "/about#mission" },
         {
-          name: "Office of Information Technology",
+          title: "Office of Information Technology",
           href: "/about#office-of-information-technology",
         },
         {
-          name: "Our Teams",
+          title: "Our Teams",
           href: "/about#our-teams",
         },
         {
-          name: "People",
+          title: "People",
           href: "/about#people",
         },
         {
-          name: "Opportunities",
+          title: "Opportunities",
           href: "/about#opportunities",
         },
         {
-          name: "Events",
+          title: "Events",
           href: "https://events.brown.edu/ccv/month",
         },
         {
-          name: "Facilities Statement",
+          title: "Facilities Statement",
           href: "/about#facilities-statement",
         },
         {
-          name: "Diversity Statement",
+          title: "Diversity Statement",
           href: "/about#diversity-statement",
         },
       ],
@@ -117,81 +122,61 @@ const NavLinks: React.FC<NavLinksProps> = ({ linkType }) => {
       href: "/blog",
     },
   ]
-
-  const paths = [
-    { slug: "/services", title: "Services" },
-    { slug: "/our-work", title: "Our Work" },
-    { slug: "/help", title: "Help" },
-    { slug: "/about", title: "About" },
-    { slug: "/blog", title: "Blog" },
-  ]
   const pathname = usePathname()
-  if (linkType === "mobile") {
-    return (
-      <>
-        {paths.map((path) => {
-          return (
-            <Menu
-              key={path.slug}
-              as="a"
-              href={path.slug}
-              className={`block border-l-4 text-lg font-medium py-2 pl-3 pr-4 hover:border-gray-300 hover:text-secondary-blue-500 ${pathname === path.slug ? "active text-secondary-blue-500 border-secondary-blue-500" : "border-transparent text-neutral-700"}`}
-            >
-              {path.title}
-            </Menu>
-          )
-        })}
-      </>
-    )
-  }
   return (
     <>
       {navigation.map((path) => {
         return (
-          <Menu key={path.href}>
-            <div className="hidden lg:ml-6 group lg:flex">
-              <Link
-                className={`inline-flex text-lg items-center border-b-2 px-1 pt-1 font-medium hover:border-gray-300 hover:text-secondary-blue-500 ${pathname === path.href ? "active text-secondary-blue-500 border-b-2 border-secondary-blue-500" : "border-transparent text-neutral-700"}`}
-                href={path.href}
-              >
-                {path.title}
-              </Link>
-              {path.routes && (
-                <MenuButton className="pt-1 group relative inline-flex items-center justify-center rounded-md text-neutral-700 hover:text-neutral-500">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open {path.title}</span>
-                  <ChevronDownIcon
-                    aria-hidden="true"
-                    className="block h-5 w-5 group-data-[open]:hidden"
-                  />
-                  <XMarkIcon
-                    aria-hidden="true"
-                    className="hidden h-5 w-5 group-data-[open]:block"
-                  />
-                </MenuButton>
-              )}
-            </div>
-            <MenuItems
-              transition
-              anchor="bottom end"
-              className="flex flex-col space-y-3 right-0 z-10 mt-6 w-44 origin-top-right rounded-md border border-white bg-neutral-50 p-2 text-sm transition duration-100 ease-out [--anchor-gap:var(--spacing-1)] focus:outline-none data-[closed]:scale-95 data-[closed]:opacity-0"
-            >
-              {/* Sub-routes */}
-              {path.routes &&
-                path.routes.map((route) => {
-                  return (
-                    <MenuItem key={route.href}>
-                      <Link
-                        href={route.href}
-                        className="inline-flex items-center px-1 pt-1 font-medium hover:text-secondary-blue-500 data-[focus]:bg-white data-[focus]:outline-none"
-                      >
-                        {route.name}
-                      </Link>
-                    </MenuItem>
-                  )
-                })}
-            </MenuItems>
-          </Menu>
+          <div key={path.href}>
+            {path.routes && (
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>{path.title}</NavigationMenuTrigger>
+                <NavigationMenuContent className="bg-neutral-50">
+                  <ul className="grid gap-1  md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                    {path.routes && (
+                      <li>
+                        <Link href={path.href} legacyBehavior passHref>
+                          <NavigationMenuLink
+                            className={navigationMenuTriggerStyle()}
+                          >
+                            Explore {path.title}
+                          </NavigationMenuLink>
+                        </Link>
+                      </li>
+                    )}
+                    {path.routes &&
+                      path.routes.map((item) => {
+                        return (
+                          <li>
+                            <Link
+                              key={item.href}
+                              href={item.href}
+                              legacyBehavior
+                              passHref
+                            >
+                              <NavigationMenuLink
+                                className={navigationMenuTriggerStyle()}
+                              >
+                                {item.title}
+                              </NavigationMenuLink>
+                            </Link>
+                          </li>
+                        )
+                      })}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            )}
+            {!path.routes && (
+              <NavigationMenuItem>
+                <Link href={path.href} legacyBehavior passHref>
+                  <NavigationMenuLink className={navigationMenuTriggerStyle()}>
+                    Documentation
+                  </NavigationMenuLink>
+                </Link>
+              </NavigationMenuItem>
+            )}
+          </div>
         )
       })}
     </>
--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,74 +1,26 @@
-import {
-  Disclosure,
-  DisclosureButton,
-  DisclosurePanel,
-} from "@headlessui/react"
+"use client"
+
 import { MagnifyingGlassIcon } from "@heroicons/react/20/solid"
 import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/outline"
 import Link from "next/link"
 import LogoCcv from "@/components/assets/LogoCcv"
 import LogoBrown from "@/components/assets/LogoBrown"
 import NavLinks from "@/components/NavLinks"
+import * as React from "react"
+
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuList,
+} from "@/components/ui/navigation-menu"
 
 const Navbar = () => {
   return (
-    <Disclosure as="nav" className="bg-neutral-50 shadow">
-      <div className="mx-auto max-w-7xl p-2 sm:px-4 lg:px-8">
-        <div className="flex flex-col gap-3.5 xl:flex-row xl:h-16 xl:justify-between">
-          <div className="flex justify-between px-2 lg:px-0">
-            <div className="flex flex-shrink-0 items-center gap-3">
-              <a href="https://it.brown.edu/" target="_blank">
-                <span className="sr-only">Brown IT Website</span>
-                <LogoBrown width={115} />
-              </a>
-              <Link href="/">
-                <span className="sr-only">Home</span>
-                <LogoCcv width={95} />
-              </Link>
-            </div>
-            <div className="hidden xl:ml-6 xl:flex xl:items-center xl:space-x-8">
-              <NavLinks />
-            </div>
-            <section className="flex items-center px-2 gap-10 space-x-4 xl:ml-6">
-              <div className="w-full max-w-lg xl:max-w-xs">
-                <label htmlFor="search" className="sr-only">
-                  Search
-                </label>
-                <div className="relative">
-                  <button className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-                    <MagnifyingGlassIcon
-                      aria-hidden="true"
-                      className="h-5 w-5 text-neutral-300"
-                    />
-                  </button>
-                </div>
-              </div>
-              <div className="flex items-center xl:hidden">
-                {/* Mobile menu button */}
-                <DisclosureButton className="group relative inline-flex items-center justify-center rounded-md p-2 text-neutral-700 ">
-                  <span className="absolute -inset-0.5" />
-                  <span className="sr-only">Open main menu</span>
-                  <Bars3Icon
-                    aria-hidden="true"
-                    className="block h-6 w-6 group-data-[open]:hidden"
-                  />
-                  <XMarkIcon
-                    aria-hidden="true"
-                    className="hidden h-6 w-6 group-data-[open]:block"
-                  />
-                </DisclosureButton>
-              </div>
-            </section>
-          </div>
-        </div>
-      </div>
-
-      <DisclosurePanel className="xl:hidden">
-        <div className="space-y-1 pb-3 pt-2">
-          <NavLinks linkType="mobile" />
-        </div>
-      </DisclosurePanel>
-    </Disclosure>
+    <NavigationMenu className="bg-neutral-50 shadow">
+      <NavigationMenuList>
+        <NavLinks />
+      </NavigationMenuList>
+    </NavigationMenu>
   )
 }
 export default Navbar
