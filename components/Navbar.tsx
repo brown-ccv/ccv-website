289867200193c4badeb6548960ea627b3faa158e
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-11-25 16:32:16 -0500

    style: gap between route trigger and chevron

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -49,8 +49,8 @@ const Navbar = () => {
           {navigation.map((item) => {
             return (
               <NavigationMenu.Item key={item.href}>
-                <NavigationMenu.Trigger className="group inline-flex h-9 items-center justify-center rounded-md bg-background px-4 py-2 text-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
-                  {item.title}{" "}
+                <NavigationMenu.Trigger className="group inline-flex h-9 items-center justify-center gap-2 rounded-md bg-background px-4 py-2 text-md font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-none disabled:pointer-events-none disabled:opacity-50 data-[active]:bg-accent/50 data-[state=open]:bg-accent/50">
+                  {item.title}
                   <ChevronDownIcon
                     className="relative top-[1px] ml-1 h-3 w-3 transition duration-300 group-data-[state=open]:rotate-180"
                     aria-hidden="true"
