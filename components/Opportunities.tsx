830b63db3b499355967686515dce11caa0399341
Heather Yu <35639529+hetd54@users.noreply.github.com>
2024-11-26 14:34:12 -0500

    style: more responsive

--- components/Opportunities.tsx
+++ components/Opportunities.tsx
@@ -71,7 +71,7 @@ export const Opportunities: React.FC = () => {
           return (
             <div
               key={position.externalPath}
-              className="flex flex-col gap-6 m-4"
+              className="flex flex-col m-4 gap-6"
             >
               <a
                 key={position.externalPath}
@@ -79,16 +79,16 @@ export const Opportunities: React.FC = () => {
                 href={`https://brown.wd5.myworkdayjobs.com/en-US/staff-careers-brown${position.externalPath}`}
               >
                 <LinkCard>
-                  <div className="flex items-center justify-between">
-                    <div>
-                      <p className="flex items-center gap-4 text-secondary-blue-700">
+                  <div className="flex flex-col gap-6 md:items-center md:justify-between md:flex-row">
+                    <div className="flex flex-col gap-2">
+                      <p className="flex items-center text-secondary-blue-700 gap-2 md:gap-4">
                         <MapPinIcon className="h-4 w-4" /> Providence, RI -
                         United States
                       </p>
                       <p className="text-gray-800">{position.title}</p>
                     </div>
 
-                    <div className="flex items-center gap-4">
+                    <div className="flex items-center gap-2 md:gap-6">
                       <p>Learn More</p>
                       <ArrowRightIcon className="h-4 w-4" />
                     </div>
