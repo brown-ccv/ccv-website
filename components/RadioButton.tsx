6b19a9580ef954f3cd21c70de1712fd0e156d308
ashleysyg <ashley.syg.lee@gmail.com>
2024-12-01 20:51:57 -0500

    fnctions to update state for radio button group

--- components/RadioButton.tsx
+++ components/RadioButton.tsx
@@ -1,12 +1,26 @@
-export default function RadioButton() {
+'use client'
+
+import { useState } from 'react'
+
+function RadioButton() {
+
+    const [selectedOption, setSelectedOption] = useState("option1")  
+
+    function onValueChange(event: any){
+        setSelectedOption(event.target.value)
+    }
+
     return (
         <div>
             <div className="flex items-center">
                 <input 
                     type="radio"
                     id="option1"
-                    name="name1"
+                    name="group"
                     value="option1"
+                    className="mt-6 flex items-center space-x-3"
+                    checked={selectedOption === "option1"}
+                    onChange={onValueChange}
                 >
                 </input>
                 <label htmlFor="option1">Option 1</label>
@@ -15,12 +29,15 @@ export default function RadioButton() {
                 <input 
                     type="radio"
                     id="option2"
-                    name="name2"
+                    name="group"
                     value="option2"
-                >
+                    checked={selectedOption === "option2"}
+                    onChange={onValueChange}                >
                 </input>
                 <label htmlFor="option2">Option 2</label>
             </div>
         </div>
     )
 }
+
+export default RadioButton
