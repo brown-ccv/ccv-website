a08c09c3a5be99cdd73639870462f8a61ba0c1c0
ashleysyg <ashley.syg.lee@gmail.com>
2024-12-09 12:26:24 -0500

    abstract radio btn attributes from toy data

--- components/RadioButton.tsx
+++ components/RadioButton.tsx
@@ -2,9 +2,14 @@
 
 import { useState } from 'react'
 
+const options = [
+    { id: 'option 1' },
+    { id: 'option 2' }
+]
+
 function RadioButton() {
 
-    const [selectedOption, setSelectedOption] = useState("option1")  
+    const [selectedOption, setSelectedOption] = useState("option 1")  
 
     function onValueChange(event: any){
         setSelectedOption(event.target.value)
@@ -12,32 +17,21 @@ function RadioButton() {
 
     return (
         <div>
-            <div className="flex items-center">
-                <input 
-                    type="radio"
-                    id="option1"
-                    name="group"
-                    value="option1"
-                    className="flex items-center form-radio text-primary-500 hover:ring hover:ring-primary-500 focus:border-gray-100 focus:bg-primary-500 focus:outline-primary-500 focus:ring focus:ring-primary-500 checked:border-gray-100 checked:bg-primary-500 disabled:border-gray-100 disabled:outline-gray-100"
-                    checked={selectedOption === "option1"}
-                    onChange={onValueChange}
-                >
-                </input>
-                <label htmlFor="option1" className="ml-3 text-md/6 text-neutral-900">Option 1</label>
-            </div>
-            <div className="flex items-center">
-                <input 
-                    type="radio"
-                    id="option2"
-                    name="group"
-                    value="option2"
-                    className="flex items-center form-radio text-primary-500 hover:ring hover:ring-primary-500 focus:border-gray-100 focus:bg-primary-500 focus:outline-primary-500 focus:ring focus:ring-primary-500 checked:border-gray-100 checked:bg-primary-500 disabled:border-gray-100 disabled:outline-gray-100"
-                    checked={selectedOption === "option2"}
-                    onChange={onValueChange}                
-                >
-                </input>
-                <label htmlFor="option2" className="ml-3 text-md/6 text-neutral-900">Option 2</label>
-            </div>
+            {options.map((option) => (
+                <div key={option.id} className="flex items-center">
+                    <input 
+                        type="radio"
+                        id={option.id}
+                        name="group"
+                        value={option.id}
+                        className="flex items-center form-radio text-primary-500 hover:ring hover:ring-primary-500 focus:border-gray-100 focus:bg-primary-500 focus:outline-primary-500 focus:ring focus:ring-primary-500 checked:border-gray-100 checked:bg-primary-500 disabled:border-gray-100 disabled:outline-gray-100"
+                        checked={selectedOption === option.id}
+                        onChange={onValueChange}
+                    >
+                    </input>
+                    <label htmlFor={option.id} className="ml-3 text-md/6 text-neutral-900">{option.id[0].toUpperCase() + option.id.slice(1)}</label>
+                </div>
+            ))}
         </div>
     )
 }
