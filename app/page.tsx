dd1de264ceeb63922725f32ac3ba9e43b97acde9
ashleysyg <ashley.syg.lee@gmail.com>
2024-12-09 14:08:08 -0500

    initial commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import EventSection from "@/components/EventSection"
+import CheckboxGroup from "@/components/CheckboxGroup"
 
 export default function Home() {
   return (
     <main className="bg-white flex min-h-screen flex-col items-center justify-between p-0 lg:p-24">
       <EventSection />
+      <CheckboxGroup />
     </main>
   )
 }
