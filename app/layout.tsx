56362f823bba9dc83e7262f7f5b16d3e8755c2c1
ashleysyg <ashley.syg.lee@gmail.com>
2025-03-28 12:56:08 -0400

    added landing page components to home

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,19 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css"
-import React from "react"
-import Header from "@/components/header/Header"
-import Footer from "@/components/Footer"
+// import React from "react"
+// import Header from "@/components/header/Header"
+// import Footer from "@/components/Footer"
+
+import React, { useRef } from "react";
+import { Header } from "@/components/Header";
+import { Hero } from "@/components/Hero";
+import { MissionCards } from "@/components/MissionCards";
+import { FeaturedProjects } from "@/components/FeaturedProjects";
+import { Footer } from "@/components/Footer";
+// import { StatusBanner } from "../../components/StatusBanner";
+// import { Navbar } from "../../components/Navbar";
+// import { Events } from "../../components/Events";
 
 const inter = Inter({ subsets: ["latin"] })
 
@@ -17,12 +27,39 @@ export default function RootLayout({
 }: Readonly<{
   children: React.ReactNode
 }>) {
+
+  const featuredProjectsRef = useRef<HTMLDivElement>(null);
+
+  const scrollToFeaturedProjects = () => {
+    featuredProjectsRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <html lang="en">
       <body className={inter.className}>
         <Header />
         <main>{children}</main>
         <Footer />
+
+        <div className="bg-white flex flex-row justify-center w-full">
+          <div className="bg-white w-full max-w-[1440px] relative">
+            {/* <StatusBanner /> */}
+            <Header />
+            {/* <Navbar /> */}
+            <div className="relative">
+              <Hero onViewEventsClick={scrollToFeaturedProjects} />
+              <MissionCards />
+            </div>
+            <div ref={featuredProjectsRef} className="mt-20">
+              <FeaturedProjects />
+            </div>
+            {/* <Events /> */}
+            <Footer />
+          </div>
+        </div>
+
+
+
       </body>
     </html>
   )
