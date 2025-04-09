af091443a4f6301e0af5cb487a70e75fe192fe61
ashleysyg <ashley.syg.lee@gmail.com>
2025-04-09 13:43:09 -0400

    chore: fix ref to events, not featured carousel (still needs to be refactored in diff PR)

--- app/ClientLayout.tsx
+++ app/ClientLayout.tsx
@@ -5,26 +5,27 @@ import React, { useRef } from "react"
 import { Hero } from "@/components/Hero"
 import { HeroCard } from "@/components/HeroCard"
 import { ImpactBanner } from "@/components/ImpactBanner"
+import SectionHeader from "@/components/SectionHeader"
 import { FeaturedCarousel } from "@/components/FeaturedCarousel"
 
 export default function ClientLayout() {
-  const featuredCarouselRef = useRef<HTMLDivElement>(null)
+  const eventsRef = useRef<HTMLDivElement>(null)
 
-  const scrollToFeaturedCarousel = () => {
-    featuredCarouselRef.current?.scrollIntoView({ behavior: 'smooth' })
+  const scrollToEvents = () => {
+    eventsRef.current?.scrollIntoView({ behavior: 'smooth' })
   }
 
   return (
     <>
       <div className="relative w-full">
           <div className="flex flex-col space-y-24"> 
-            <Hero onViewEventsClick={scrollToFeaturedCarousel} />
+            <Hero onViewEventsClick={scrollToEvents} />
             <HeroCard />
             <ImpactBanner />
           </div>
         </div>
-      <div ref={featuredCarouselRef} className="mt-20 w-full">
-        <FeaturedCarousel />
+      <div ref={eventsRef} className="mt-20 w-full">
+        <SectionHeader  href={"#events"} title={"Events"} />
       </div>
     </>
   )
