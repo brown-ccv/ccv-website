72b726198b78e2a6ffa8ba8fe4d4c7dec6836c0c
George Dang <53052793+gtdang@users.noreply.github.com>
2025-04-10 10:43:02 -0400

    feat: added image prop to Hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,11 +1,17 @@
 import { ReactNode } from "react"
 
-export const Hero = ({ children }: { children: ReactNode }) => {
+export const Hero = ({
+  image,
+  children,
+}: {
+  image: string
+  children: ReactNode
+}) => {
   return (
     <div
       className="w-full h-[1200px] bg-cover bg-center relative flex flex-col overflow-hidden m-0 p-0"
       style={{
-        backgroundImage: `url(/static/images/hero-landing.jpeg)`,
+        backgroundImage: image,
         backgroundSize: "cover",
       }}
     >
