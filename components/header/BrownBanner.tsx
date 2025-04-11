3371f10b39cefe286a25fc9b9fce9ab752628586
George Dang <53052793+gtdang@users.noreply.github.com>
2025-04-11 14:53:44 -0400

    refactor: added separator boarder, resized, and regrouped components

--- components/header/BrownBanner.tsx
+++ components/header/BrownBanner.tsx
@@ -5,28 +5,27 @@ import BrownLogo from "@/components/assets/BrownLogo"
 
 export const BrownBanner: React.FC = () => {
   return (
-    <header className="w-full h-[149px] bg-white relative mt-10 flex items-center px-6">
-      <div className="flex items-center w-full">
-        <BrownLogo className="w-[200px] h-auto" />
-        <div className="ml-20 flex flex-col justify-center">
-          <h1 className="font-semibold text-2xl">
+    <header className="w-full h-[125px] bg-white relative mt-10 flex items-center px-6">
+      <div className="flex flex-row items-center w-full">
+        <BrownLogo />
+        <div className="ml-6 w-full flex flex-row justify-between items-center">
+          <h1 className="py-7 pl-6 h-full sm:text-xl lg:text-2xl font-semibold border-l border-black md:block">
             Center for Computation and Visualization
           </h1>
+          <Button
+            variant="outline_primary"
+            className="border-red-university text-red-university font-semibold rounded-none w-[159px] h-[50px] hover:bg-red-university hover:text-white hover:border-red-university"
+          >
+            <a
+              href="mailto:ccv-support@brown.edu"
+              className="flex items-center justify-center w-full h-full"
+            >
+              <MessagesSquareIcon className="mr-2 h-[26px] w-[26px]" />
+              Work with Us
+            </a>
+          </Button>
         </div>
       </div>
-
-      <Button
-        variant="outline_primary"
-        className="absolute right-[49px] top-[50%] transform -translate-y-1/2 border-red-university text-red-university font-semibold rounded-none w-[159px] h-[50px] hover:bg-red-university hover:text-white hover:border-red-university"
-      >
-        <a 
-          href="mailto:ccv-support@brown.edu" 
-          className="flex items-center justify-center w-full h-full"
-        >
-          <MessagesSquareIcon className="mr-2 h-[26px] w-[26px]" />
-          Work with Us
-        </a>
-      </Button>
     </header>
   )
 }
