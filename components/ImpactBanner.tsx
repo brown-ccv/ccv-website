2b9eda7d53f9938fce2b42efd2b5b613b9d539f1
ashleysyg <ashley.syg.lee@gmail.com>
2025-04-17 15:18:05 -0400

    chore: impact banner flex behavior for small screens

--- components/ImpactBanner.tsx
+++ components/ImpactBanner.tsx
@@ -9,21 +9,21 @@ export const ImpactBanner = (): JSX.Element => {
   ]
 
   return (
-    <div className="w-full bg-gray-100">
+    <div className="w-full h-auto bg-gray-100">
       <Card className="w-full border-none shadow-none rounded-none bg-transparent">
-        <CardContent className="max-w-[1440px] mx-auto h-[180px] flex items-center px-6 py-10">
-          <div className="flex-shrink-0 mr-32">
-            <h2 className="font-semibold text-defaultblack text-[28px] font-sans">
-              The CCV Impact
-            </h2>
-            <p className="italic text-gray-600 text-xl mt-2 font-serif">
-              As of January 1, 2025
-            </p>
-          </div>
+        <CardContent className="max-w-[1440px] mx-auto max-h-[600px] flex items-center px-6 py-10">
+           <div className="flex flex-wrap flex-col md:flex-row justify-between flex-grow">
+            <div className="flex flex-col items-center">
+              <h2 className="font-semibold text-black text-[28px] font-sans">
+                The CCV Impact
+              </h2>
+              <p className="italic text-gray-600 text-xl font-serif">
+                As of January 1, 2025
+              </p>    
+            </div>
 
-          <div className="flex justify-between flex-grow">
             {impactMetrics.map((metric, index) => (
-              <div key={index} className="flex flex-col items-center">
+              <div key={index} className="flex flex-col items-center mt-6 md:mt-0">
                 <div
                   className="bg-gradient-to-br from-purple-900 to-pink-500 bg-clip-text text-transparent
                   font-bold text-6xl text-center font-sans"
