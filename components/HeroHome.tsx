279ac3e721b704e023e5e0c81e9cdb7bfdb054b4
ashley <ashley_lee@brown.edu>
2025-04-21 13:37:45 -0400

    feat: add center for computation and visualization to hero

--- components/HeroHome.tsx
+++ components/HeroHome.tsx
@@ -11,6 +11,10 @@ export const HeroHome = () => {
       {/* Hero content */}
       <div className="relative flex-1 flex items-start px-[8vw]">
         <div className="absolute top-[12%] flex flex-col w-full text-white space-y-8">
+          <h2 className="font-semibold text-keppel-500 text-2xl tracking-tighter">
+            Center for Computation and Visualization
+          </h2>
+
           <TypingAnimation className="font-bold text-4xl md:text-5xl lg:text-6xl max-w-full">
             Advancing Computational Research
           </TypingAnimation>
