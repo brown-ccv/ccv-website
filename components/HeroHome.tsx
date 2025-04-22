a77f6e5edfd5fb77c7a3c0e974b1ad82b5431a7e
ashley <ashley_lee@brown.edu>
2025-04-22 14:40:08 -0400

    fix: hero text spillover, button width and flex behavior, consistent top margins for buttons, apply zoom across all screen sizes

--- components/HeroHome.tsx
+++ components/HeroHome.tsx
@@ -10,34 +10,36 @@ export const HeroHome = () => {
     <div className="bg-blue-navbar">
       <Hero image={"static/images/hero-landing.jpeg"}>
         {/* Hero content */}
-        <div className="relative flex-1 flex items-start content-wrapper overflow-hidden">
+        <div className="relative flex-1 flex items-start w-full px-24 overflow-hidden">
           {/* Removed w-full here to prevent spillover */}
           <div className="absolute top-[12%] flex flex-col max-w-full text-white space-y-6">
-          <TypingAnimation className="font-bold text-4xl md:text-5xl lg:text-6xl">
+          <TypingAnimation className="font-bold text-7xl">
               Center for Computation and Visualization
             </TypingAnimation>
 
-            <p className="text-lg md:text-xl font-normal">
+            <p className="text-2xl font-normal">
               Advancing computational research with scientific and computing
               expertise.
             </p>
 
-            <div className="flex flex-col sm:flex-row gap-4 pt-0 md:pt-20">
+            <div className="flex flex-row flex-wrap gap-4 pt-8 md:pt-20">
               <Button
                 variant="secondary_filled"
-                className="h-[55px] font-semibold"
+                className="h-[55px] min-w-[155px] self-start"
               >
                 Work with Us
               </Button>
 
               <Button
                 variant="secondary_outlined"
-                className="h-[55px] font-semibold"
+                className="h-[55px] min-w-[155px] self-start"
                 onClick={() => scrollToID("events")}
               >
                 View Events
               </Button>
             </div>
+
+
           </div>
         </div>
       </Hero>
