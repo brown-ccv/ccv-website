1293a056e89b2abc402f116daa9b9727efefd2dc
George Dang <53052793+gtdang@users.noreply.github.com>
2025-04-30 15:40:54 -0400

    refactor: remove redundant class name from TooltipContent

--- components/ui/tooltip.tsx
+++ components/ui/tooltip.tsx
@@ -20,7 +20,7 @@ const TooltipContent = React.forwardRef<
       ref={ref}
       sideOffset={sideOffset}
       className={cn(
-        "TooltipContent z-50 overflow-hidden rounded-md bg-neutral-50 px-3 py-1.5 text-lg",
+        "z-50 overflow-hidden rounded-md bg-neutral-50 px-3 py-1.5 text-lg",
         className
       )}
       {...props}
