13cac4dc3ec6edd5bed588579b45b7c6a1b696dc
ashley <ashley_lee@brown.edu>
2025-05-06 16:30:04 -0400

    feat: move images into public bc next standard, update hero image for routes

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
       <div className="w-full">
         <div className="relative w-full flex flex-col">
         <div className="bg-blue-navbar">
-            <Hero image={"static/images/ccv-original.png"}>
+            <Hero image={"/images/ccv-original.png"}>
                   <div className="relative flex-1 flex items-start w-full px-6 md:px-24 bg-gradient-to-t from-black/0 via-black/10 to-black/65 z-5">
                     <div className="absolute top-[12%] flex flex-col text-white space-y-6">
                       <TextAnimate className="font-bold text-6xl md:text-8xl">
@@ -52,7 +52,6 @@ export default async function Home() {
                     </div>
                   </div>
               </Hero>
-
         </div>
           <HeroCard />
           <ImpactBanner />
