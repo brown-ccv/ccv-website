5964c4894f26882e9d6261a11d928adf345014e4
George Dang <53052793+gtdang@users.noreply.github.com>
2025-06-02 17:06:15 -0400

    refactor: update CardWithImage component

--- components/ui/people-card.tsx
+++ components/ui/people-card.tsx
@@ -1,26 +1,42 @@
 // components/ui/card-with-image.tsx
-"use client";
+"use client"
 
-import React, { useState } from "react";
-import Image from "next/image";
-import { Card, CardContent, CardTitle, CardDescription } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { cardVariants } from "@/components/ui/variants";
+import React, { useState } from "react"
+import Image from "next/image"
+import {
+  Card,
+  CardContent,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+import { cardVariants } from "@/components/ui/variants"
 
 interface CardWithImageProps {
-  className?: string;
-  imagePath: string;
-  hoverImagePath?: string;
-  name: string;
-  title: string;
+  className?: string
+  imagePath: string
+  hoverImagePath?: string
+  name: string
+  title: string
 }
 
-export const CardWithImage: React.FC<CardWithImageProps> = ({ className, imagePath, hoverImagePath, name, title, ...props }) => {
-  const [isHovered, setIsHovered] = useState(false);
+export const CardWithImage: React.FC<CardWithImageProps> = ({
+  className,
+  imagePath,
+  hoverImagePath,
+  name,
+  title,
+  ...props
+}) => {
+  const [isHovered, setIsHovered] = useState(false)
 
   return (
     <Card
-      className={cn(className, "overflow-hidden", cardVariants({ variant: "people" }))}
+      className={cn(
+        className,
+        "overflow-hidden",
+        cardVariants({ variant: "people" })
+      )}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
@@ -31,15 +47,17 @@ export const CardWithImage: React.FC<CardWithImageProps> = ({ className, imagePa
             alt={name}
             width={300}
             height={300}
-            style={{ objectFit: 'cover' }}
+            style={{ objectFit: "cover" }}
             className="rounded-full w-full transition-opacity duration-300"
           />
         </div>
         <div>
           <CardTitle className="text-2xl text-center py-4">{name}</CardTitle>
-          <CardDescription className="text-xl italic text-center">{title}</CardDescription>
+          <CardDescription className="text-xl italic text-center">
+            {title}
+          </CardDescription>
         </div>
       </CardContent>
     </Card>
-  );
-};
+  )
+}
