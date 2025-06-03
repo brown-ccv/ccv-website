8f92ea37e6c107fbb2d44698dbe9a697aa979a0a
George Dang <53052793+gtdang@users.noreply.github.com>
2025-06-03 16:44:26 -0400

    feat: update mission statement in About Us page for CBC

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -134,21 +134,16 @@ export default async function AboutUs() {
         <Card className="w-full border-none shadow-none rounded-none">
           <CardContent className="max-w-[1440px] mx-auto max-h-[600px] flex items-center px-6 py-10">
             <p className="text-black text-xl">
-              We envision an environment where computational best practices,
-              innovative solutions, and expert knowledge combine to build
-              advanced tools for research and to enable new discoveries. Our
-              mission is to provide the scientific and technical computing
-              expertise required to advance computational research and support
-              Brown’s academic mission. In practice, this frequently means
-              partnering with researchers for projects that may span weeks,
-              months, or years. In some cases, these partnerships can involve
-              researchers using grant funds as partial support of one of our
-              research software engineers or data scientists. We have a team of
-              data scientists and research software engineers with a huge
-              variety of scientific backgrounds (e.g., Engineering, Physics,
-              Computer Vision, Biology, Psychology, Statistics, Applied Math,
-              Computer Science, etc.), so we can closely calibrate a person with
-              a project.
+              The primary mission of the Computational Biology Core (CBC) is to
+              provide support to junior investigators in the analysis and
+              interpretation of high-throughput DNA/RNA sequencing datasets,
+              encompassing both internally generated and publicly accessible
+              data. The Core is also committed to facilitating scientific
+              collaboration among COBRE projects. The long-term objective of the
+              CBC is to establish a sustainable resource that addresses the
+              evolving data analysis needs of genomic research across Brown
+              University and its affiliated hospitals, complemented by training
+              initiatives to develop the next cohort of junior investigators.
             </p>
           </CardContent>
         </Card>
