52be29842bf9e51d7b55c327422a1276c2b81fac
George Dang <53052793+gtdang@users.noreply.github.com>
2025-06-03 17:39:11 -0400

    refactor: reorganized public images folder

--- app/page.tsx
+++ app/page.tsx
@@ -28,7 +28,7 @@ export default async function Home() {
       <div className="w-full">
         <div className="relative w-full flex flex-col">
           <div className="bg-blue-navbar">
-            <Hero image={"/static/images/dna-dark-mirror.svg"}>
+            <Hero image={"/images/dna-dark-mirror.svg"}>
               <div className="relative flex-1 flex items-start w-full px-6 md:px-24 bg-gradient-to-t from-black/0 via-black/10 to-black/65 z-5">
                 <div className="absolute top-[12%] flex flex-col text-white space-y-6">
                   <TextAnimate className="font-bold text-5xl md:text-8xl">
