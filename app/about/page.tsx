fa9b3c716cb0b56c5673daea3aa7f54f4c338c39
George Dang <53052793+gtdang@users.noreply.github.com>
2025-06-03 17:39:48 -0400

    feat: update description in About Us page to reflect genomic discovery focus

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -83,10 +83,8 @@ export default async function AboutUs() {
                   About Us
                 </TextAnimate>
                 <p className="text-4xl font-semibold leading-[1.5]">
-                  The Center for Computation and Visualization provides
-                  high-performance computing and visualization services to the
-                  Brown community. We also collaborate with researchers on
-                  projects across vast range of disciplines.
+                  We empower genomic discovery through expert data analysis and
+                  investigator support.
                 </p>
               </div>
             </div>
