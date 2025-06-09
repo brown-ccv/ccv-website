90a0d8ea118a0c4396ec2022672b2594a469a23d
ashley <ashley_lee@brown.edu>
2025-06-09 10:51:05 -0400

    chore: changes hero background color to blue-navbar from purple-900

--- app/about/careers/page.tsx
+++ app/about/careers/page.tsx
@@ -14,7 +14,7 @@ export default async function AboutLayout() {
 
     return (
       <div className="w-full">
-        <div className="bg-purple-900">
+        <div className="bg-blue-navbar">
             <Hero image={"/images/hero/about-3.png"}>
                 <div className="relative flex-1 flex items-start w-full px-6 md:px-24 bg-gradient-to-t from-black/0 via-black/10 to-black/65 z-5">
                     <div className="absolute top-[12%] flex flex-col text-white space-y-6 w-[80vw]">
--- app/services/virtual-machine-hosting/page.tsx
+++ app/services/virtual-machine-hosting/page.tsx
@@ -6,7 +6,7 @@ export default async function VirtualMachineHosting() {
     return (
       <div className="w-full">
         <div className="relative w-full flex flex-col">
-          <div className="bg-purple-900">
+          <div className="bg-blue-navbar">
             <Hero image={"/images/hero/hero-subroutes.jpeg"}>
                   <div className="relative flex-1 flex items-start w-full px-6 md:px-24 bg-gradient-to-t from-black/0 via-black/10 to-black/65 z-5">
                     <div className="absolute top-[12%] flex flex-col text-white space-y-6">
