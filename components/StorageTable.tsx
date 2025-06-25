f1bf6b4686f49788d9d7af9fbc83761834b3ffaa
ashley <ashley_lee@brown.edu>
2025-06-25 09:09:45 -0400

    fix(style): update icons for data protection features, replace Undefined with "-"

--- components/StorageTable.tsx
+++ components/StorageTable.tsx
@@ -134,6 +134,15 @@ const tableData: TableRow[] = useMemo(() => {
           const feature = info.getValue() as ServiceFeature;
           const featureNameFromRow = info.row.original.featureName;
           const cellContentClasses = cn("px-4 py-2 text-start", columnClass);
+
+          if (feature === undefined) {
+            return (
+              <div className={cn("flex flex-col items-center justify-center p-2 min-h-[80px]", cellContentClasses)}>
+                <span className="text-neutral-400 text-3xl">-</span>
+              </div>
+            );
+          }
+
           const featureClassLower = String(feature?.value).toLowerCase();
           const IconComponent = featureIcons[featureNameFromRow.toLowerCase()];
           const valueColor = featureColorMap[featureClassLower] || featureColorMap['default'];
@@ -203,12 +212,13 @@ const tableData: TableRow[] = useMemo(() => {
           <thead className="bg-white">
             {table.getHeaderGroups().map(headerGroup => (
               <tr key={headerGroup.id}>
-                {headerGroup.headers.map(header => (
+                {headerGroup.headers.map((header, index) => (
                   <th
                     key={header.id}
                     className={cn(
                       "px-2 py-3 text-left text-md font-medium text-neutral-500 uppercase tracking-wider min-w-[175px]",
-                      header.column.getCanSort() ? 'cursor-pointer select-none' : ''
+                      header.column.getCanSort() ? 'cursor-pointer select-none' : '',
+                      index < headerGroup.headers.length - 1 && 'border-r border-neutral-200'
                     )}
                   >
                     {flexRender(
@@ -223,8 +233,11 @@ const tableData: TableRow[] = useMemo(() => {
           <tbody className="bg-white divide-y divide-neutral-200">
             {table.getRowModel().rows.map(row => (
               <tr key={row.id}>
-                {row.getVisibleCells().map(cell => (
-                  <td key={cell.id} className="py-2">
+                {row.getVisibleCells().map((cell, index) => (
+                  <td key={cell.id}                     className={cn(
+                    "py-2",
+                    index < row.getVisibleCells().length - 1 && 'border-r border-neutral-200'
+                  )}>
                     {flexRender(
                         cell.column.columnDef.cell,
                         cell.getContext()
