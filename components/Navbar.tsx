674488f39e33a7ca3ee1525a33adaba83ea8c686
ashley <ashley_lee@brown.edu>
2025-06-26 19:07:43 -0400

    chore: cleans up duplicate rates

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -81,12 +81,6 @@ const routes: NavSection[] = [
               "We provide technology and training for teaching with code",
             icon: FaUserGraduate,
           },
-          {
-            name: "Service Rates",
-            href: "/services/rates",
-            description: "Learn about the cost of our services",
-            icon: FaDollarSign,
-          },
         ],
       },
       {
@@ -118,9 +112,9 @@ const routes: NavSection[] = [
             icon: FaDesktop,
           },
           {
-            name: "Hardware Rates",
-            href: "/services/hardware-rates",
-            description: "Learn about the cost of our hardware services",
+            name: "Rates",
+            href: "/services/rates",
+            description: "Learn about the cost of our services",
             icon: FaDollarSign,
           },
         ],
