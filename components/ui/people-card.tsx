78faf913ffa2e83f4f97b13b1ff8c31d50333d9e
ashley <ashley_lee@brown.edu>
2025-07-03 00:03:38 -0400

    fix: fixed width still needed so cards do not srhink or stretch, but fixed height removed

--- components/ui/people-card.tsx
+++ components/ui/people-card.tsx
@@ -20,18 +20,18 @@ export const CardWithImage: React.FC<CardWithImageProps> = ({ className, imagePa
 
   return (
     <Card
-      className={cn("overflow-hidden", cardVariants({ variant: "people" }), "w-[400px]", "h-[600px]", "flex-shrink-0")}
+      className={cn("overflow-hidden", cardVariants({ variant: "people" }), "w-[400px]","flex-shrink-0")}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       <CardContent className="flex flex-col h-full">
-        <div className="relative overflow-hidden">
+        <div className="relative overflow-hidden flex justify-center">
           <Image
             src={isHovered && hoverImagePath ? hoverImagePath : imagePath}
             alt={name}
             width="500"
             height="500"
-            className="rounded-full transition-opacity duration-300 max-h-[350px] min-h-[350px] max-w-[350px]"
+            className="rounded-full transition-opacity duration-300 max-h-[200px] min-h-[200px] max-w-[200px] sm:max-h-[250px] sm:min-h-[250px] sm:max-w-[250px] md:max-h-[300px] md:min-h-[300px] md:max-w-[300px] lg:max-h-[350px] lg:min-h-[350px] lg:max-w-[350px]"
           />
         </div>
         <div>
