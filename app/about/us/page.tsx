3f8284ce02c30ad9c79c0edfbc3f4deaa92c1c7e
ashley <ashley_lee@brown.edu>
2025-07-03 00:02:38 -0400

    fix: padding for all sizes, remove fixed person card height to reduce space between people in smaller screens

--- app/about/us/page.tsx
+++ app/about/us/page.tsx
@@ -31,7 +31,7 @@ export default async function AboutUs() {
         <section className="content-wrapper py-24">
           <SectionHeader title="Office of Information Technology" align="center" />
           <Card className="w-full border-none shadow-none rounded-none">
-            <CardContent className="max-w-[1440px] mx-auto max-h-[600px] flex items-center px-6 py-10">
+            <CardContent className="max-w-[1440px] mx-auto flex items-center px-6 py-10">
               <p className="text-black text-xl">
                 The Center for Computation and Visualization (CCV) is a center within the University&apos;s central IT organization, which is the <a href="https://it.brown.edu" target="_blank" rel="noopener noreferrer">Office of Information Technology (OIT)</a>. In addition to building and maintaining the University&apos;s <a href="https://it.brown.edu/tools-services" target="_blank" rel="noopener noreferrer">hundreds of enterprise software, systems, and hardware</a>, OIT is also responsible for driving the technological progress that enables scientific research. Executing on the University&apos;s research mission is the key role that CCV plays in OIT.
               </p>
@@ -43,7 +43,7 @@ export default async function AboutUs() {
         <section className="content-wrapper py-24 bg-gray-100">
           <SectionHeader title="Our Mission" align="center" />
           <Card className="w-full border-none shadow-none rounded-none">
-            <CardContent className="max-w-[1440px] mx-auto max-h-[600px] flex items-center px-6 py-10">
+            <CardContent className="max-w-[1440px] mx-auto flex items-center px-6 py-10">
               <p className="text-black text-xl">
                 We envision an environment where computational best practices, innovative solutions, and expert knowledge combine to build advanced tools for research and to enable new discoveries. Our mission is to provide the scientific and technical computing expertise required to advance computational research and support Brown's academic mission. In practice, this frequently means partnering with researchers for projects that may span weeks, months, or years. In some cases, these partnerships can involve researchers using grant funds as partial support of one of our research software engineers or data scientists. We have a team of data scientists and research software engineers with a huge variety of scientific backgrounds (e.g., Engineering, Physics, Computer Vision, Biology, Psychology, Statistics, Applied Math, Computer Science, etc.), so we can closely calibrate a person with a project.
               </p>
@@ -55,7 +55,7 @@ export default async function AboutUs() {
         <div className="content-wrapper py-16 sm:py-24">
           <SectionHeader title="People" align="center"></SectionHeader>
             <div className="flex justify-center">
-              <div className="flex flex-wrap justify-center gap-y-6 xs:w-1/2">
+              <div className="flex flex-wrap justify-center sm:gap-y-0 md:gap-y-6 xs:w-1/2">
               {pageContent?.people.map((person: PeopleTypes) => (
                 <div key={person.name}>
                   <CardWithImage 
@@ -74,7 +74,7 @@ export default async function AboutUs() {
       <section className="content-wrapper py-24 bg-gray-100">
       <SectionHeader title="Diversity Statement" align="center" />
         <Card className="w-full border-none shadow-none rounded-none">
-          <CardContent className="max-w-[1440px] mx-auto max-h-[600px] flex items-center px-6 py-10">
+          <CardContent className="max-w-[1440px] mx-auto flex items-center px-6 py-10">
             <p className="text-black text-xl">
               CCV embraces a community enriched and enhanced by diverse dimensions, including race, ethnicity and national origins, disability status, gender and gender identity, sexuality, class and religion. We believe diversity brings innovation and progress. We are especially committed to increasing the representation of those populations that have been historically underrepresented in STEM.
               <br/><br/>
