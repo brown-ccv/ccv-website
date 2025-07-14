866fadd5f0d6279328f9ed3afa77f0cc57c8e675
ashley <ashley_lee@brown.edu>
2025-07-14 10:43:04 -0400

    tofix: mdx-remote library is not compatible with react 19. mdx next library can only read content from within /app folder. create separate PR to move /content into /app and update other pages

--- app/services/stronghold/page.tsx
+++ app/services/stronghold/page.tsx
@@ -1,7 +1,26 @@
 import React from "react"
 import { Hero } from "@/components/Hero"
+import { SectionHeader } from '@/components/ui/section-header';
+import { Button } from '@/components/ui/button';
+import { ExternalLink } from '@/components/ui/external-link';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
+import { serialize } from 'next-mdx-remote/serialize';
+import fs from 'fs';
+import path from 'path';
+import matter from 'gray-matter';
+
+const components = {
+  SectionHeader,
+  Button,
+  ExternalLink,
+};
 
 export default async function Stronghold() {
+  const filePath = path.join(process.cwd(), 'content/services/stronghold/stronghold.mdx');
+  const file = fs.readFileSync(filePath, 'utf8');
+  const { content, data } = matter(file);
+  const mdxSource: MDXRemoteSerializeResult = await serialize(content);
+
   return (
     <div className="w-full">
       <div className="relative w-full flex flex-col">
@@ -9,11 +28,14 @@ export default async function Stronghold() {
           <Hero 
             image={"/images/hero/hero.jpeg"}
             title="Stronghold"
-            description="Text tbd."
+            description="Stronghold is a secure computing and storage environment that enables Brown researchers to analyze sensitive data while complying with regulatory or contractual requirements"
             titleClassName="font-bold text-6xl md:text-8xl"
           />
         </div>
       </div>
+      <div className="prose">
+        <MDXRemote {...mdxSource} components={components} />
+      </div>    
     </div>
   )
 }
