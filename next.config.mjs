c3a58fb95e3904b9f2213da9ef1f6be91979216d
ashley <ashley_lee@brown.edu>
2025-07-15 13:13:36 -0400

    build: adds build commands to update when content is updated

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,16 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {basePath: ""};
+import createMDX from '@next/mdx'
 
-export default nextConfig
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  basePath: "",
+  pageExtensions: ['js', 'jsx', 'mdx', 'ts', 'tsx'],
+}
+
+const withMDX = createMDX({
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+})
+
+export default withMDX(nextConfig)
