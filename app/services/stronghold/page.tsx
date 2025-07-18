298c6864c5fdd9de061abe14b22005749ff6a064
ashley <ashley_lee@brown.edu>
2025-07-18 13:33:35 -0400

    fix?: exporting metadata from mdx

--- app/services/stronghold/page.tsx
+++ app/services/stronghold/page.tsx
@@ -1,16 +1,15 @@
 import { Hero } from "@/components/Hero";
-import StrongholdContent, { heroTitle, heroDescription } from "@/content/services/stronghold.mdx";
+import StrongholdContent, { title, description } from "@/content/services/stronghold.mdx";
 
 export default function Stronghold() {
   return (
     <div>
-      <Hero title={heroTitle} description={heroDescription} />
+      <Hero title={title} description={description} />
       <section className="content-wrapper py-24 px-14 lg:px-36">
         <div className='prose prose-lg text-xl max-w-none'>
         <StrongholdContent />
         </div>
       </section>
-      
     </div>
   );
 }
