e0219efd2f4eb1899ba060f167a44285da45a366
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-07-24 11:05:23 -0400

    chore: home page passes pa11y

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -38,10 +38,10 @@ interface RouteItem {
 
 interface NavigationProps {
   groups: {
-    name: string;
-    routes: RouteItem[];
-  }[];
-  parentTitle: string;
+    name: string
+    routes: RouteItem[]
+  }[]
+  parentTitle: string
 }
 
 type Route = {
@@ -172,65 +172,70 @@ const routes: NavSection[] = [
           {
             name: "About Us",
             href: "/about/us",
-            description:"",
+            description: "",
             icon: FaUser,
           },
           {
             name: "Contact Us",
             href: "/about/contact",
-            description:"",
+            description: "",
             icon: FaComments,
           },
           {
             name: "Careers",
             href: "/about/careers",
-            description:"",
+            description: "",
             icon: FaUserPlus,
           },
         ],
       },
     ],
-  }
+  },
 ]
 
 export const Navbar: React.FC = () => {
   const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false)
-  const [openSubmenus, setOpenSubmenus] = useState<string[]>([]);
+  const [openSubmenus, setOpenSubmenus] = useState<string[]>([])
 
   const toggleMobileMenu = () => {
     setIsMobileMenuOpen(!isMobileMenuOpen)
     // Prevent body scrolling when mobile menu is open
-    document.body.style.overflow = isMobileMenuOpen ? 'auto' : 'hidden';
+    document.body.style.overflow = isMobileMenuOpen ? "auto" : "hidden"
   }
 
   // Effect to clean up body overflow style when component unmounts
   useEffect(() => {
     return () => {
-      document.body.style.overflow = 'auto'; // ensures body scrolling is re-enabled
-    };
-  }, []);
+      document.body.style.overflow = "auto" // ensures body scrolling is re-enabled
+    }
+  }, [])
 
   const toggleSubmenu = (sectionName: string) => {
     if (openSubmenus.includes(sectionName)) {
-      setOpenSubmenus(openSubmenus.filter(name => name !== sectionName));
+      setOpenSubmenus(openSubmenus.filter((name) => name !== sectionName))
     } else {
-      setOpenSubmenus([...openSubmenus, sectionName]);
+      setOpenSubmenus([...openSubmenus, sectionName])
     }
-  };
+  }
 
   return (
     <header className={`sticky top-0 z-50`}>
-      <nav className="bg-blue-navbar flex px-8 justify-between">
+      <nav
+        role="navigation"
+        aria-label="Main menu"
+        className="bg-blue-navbar flex px-8 justify-between"
+      >
         <div className="flex items-center py-8 px-6 xl:px-10">
           <Link href={"/"}>
-            <CCVLogo width={120}/>
+            <span className="sr-only">CCV Home</span>
+            <CCVLogo width={120} />
           </Link>
         </div>
 
         {/* Navigation Menu for Desktop */}
         <NavigationMenu.Root className="hidden lg:flex relative z-10 w-screen justify-between items-stretch">
           <NavigationMenu.List className="m-0 flex list-none rounded-md h-full items-center">
-            {routes.map((section) =>
+            {routes.map((section) => (
               <NavigationMenu.Item key={section.name}>
                 <NavigationMenu.Trigger className="group inline-flex h-9 items-center justify-center gap-2 px-3 xl:px-6 text-white text-2xl font-semibold transition-colors hover:text-sunglow-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50">
                   {section.name}
@@ -244,11 +249,11 @@ export const Navbar: React.FC = () => {
                   parentTitle={section.name}
                 />
               </NavigationMenu.Item>
-            )}
+            ))}
 
             {/* <NavigationMenu.Item> */}
-              {/* TODO: Add blog */}
-              {/* <NavigationMenu.Link
+            {/* TODO: Add blog */}
+            {/* <NavigationMenu.Link
                 className="inline-flex h-9 items-center justify-center gap-2 px-2 xl:px-4 text-white font-semibold text-2xl transition-colors hover:text-sunglow-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
                 href="/blog">
                 Blog
@@ -262,7 +267,8 @@ export const Navbar: React.FC = () => {
                 href="/about/contact"
                 className="inline-flex h-9 items-center justify-center gap-2 px-2 xl:px-4 text-white font-semibold text-2xl transition-colors hover:text-sunglow-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
               >
-                <FaQuestionCircle className="text-2xl mr-0 stroke-[2.5]"/>Help
+                <FaQuestionCircle className="text-2xl mr-0 stroke-[2.5]" />
+                Help
               </Link>
             </NavigationMenu.Item>
 
@@ -273,7 +279,8 @@ export const Navbar: React.FC = () => {
                 external={true}
                 className="inline-flex h-9 items-center justify-center gap-2 px-2 xl:px-4 text-white font-semibold text-2xl transition-colors hover:text-sunglow-400 focus:outline-none disabled:pointer-events-none disabled:opacity-50"
               >
-                <FaFileLines size="" className="text-2xl mr-0 stroke-[2.5]" />Docs
+                <FaFileLines size="" className="text-2xl mr-0 stroke-[2.5]" />
+                Docs
               </ExternalLink>
             </NavigationMenu.Item>
 
@@ -291,8 +298,15 @@ export const Navbar: React.FC = () => {
 
         {/* Mobile Menu Hamburger Button */}
         <div className="lg:hidden flex items-center">
-          <Button variant="secondary_filled" size="icon" className="text-blue-navbar rounded-2xl" onClick={toggleMobileMenu}>
-            <FaBars className="h-6 w-6" />
+          <Button
+            aria-label="Main Menu"
+            aria-controls="main-menu"
+            variant="secondary_filled"
+            size="icon"
+            className="text-blue-navbar rounded-2xl"
+            onClick={toggleMobileMenu}
+          >
+            <FaBars aria-hidden={true} focusable={false} className="h-6 w-6" />
           </Button>
         </div>
 
@@ -320,14 +334,16 @@ export const Navbar: React.FC = () => {
                   className="flex items-center justify-between w-full text-sunglow-400 font-semibold text-2xl py-7 px-6 mr-6 hover:bg-sunglow-400 hover:text-black active:bg-sunglow-200"
                 >
                   {section.name}
-                  <FaChevronDown className={`h-8 w-8 transition-transform ${openSubmenus.includes(section.name) ? 'rotate-180' : ''}`} />
+                  <FaChevronDown
+                    className={`h-8 w-8 transition-transform ${openSubmenus.includes(section.name) ? "rotate-180" : ""}`}
+                  />
                 </button>
                 {openSubmenus.includes(section.name) && (
                   <div className="ml-6">
-                    {section.groups.map(group => (
+                    {section.groups.map((group) => (
                       <div className="py-2" key={group.name}>
-                        {group.routes.map(route => (
-                          route.href.startsWith('http') ? (
+                        {group.routes.map((route) =>
+                          route.href.startsWith("http") ? (
                             <ExternalLink
                               key={route.href}
                               href={route.href}
@@ -357,24 +373,28 @@ export const Navbar: React.FC = () => {
                               )}
                             </Link>
                           )
-                        ))}
+                        )}
                       </div>
                     ))}
                   </div>
                 )}
               </div>
             ))}
-             <Link href="/about/contact" onClick={toggleMobileMenu} className="block text-sunglow-400 font-semibold text-2xl py-7 px-6 hover:bg-sunglow-400 hover:text-black active:bg-sunglow-200">
-               Help
-             </Link>
-             <ExternalLink
-               href="https://docs.ccv.brown.edu/documentation"
-               external
-               onClick={toggleMobileMenu}
-               className="block text-sunglow-400 font-semibold text-2xl py-7 pl-6 hover:bg-sunglow-400 hover:text-black active:bg-sunglow-200"
-             >
-               Docs
-             </ExternalLink>
+            <Link
+              href="/about/contact"
+              onClick={toggleMobileMenu}
+              className="block text-sunglow-400 font-semibold text-2xl py-7 px-6 hover:bg-sunglow-400 hover:text-black active:bg-sunglow-200"
+            >
+              Help
+            </Link>
+            <ExternalLink
+              href="https://docs.ccv.brown.edu/documentation"
+              external
+              onClick={toggleMobileMenu}
+              className="block text-sunglow-400 font-semibold text-2xl py-7 pl-6 hover:bg-sunglow-400 hover:text-black active:bg-sunglow-200"
+            >
+              Docs
+            </ExternalLink>
           </div>
         )}
       </nav>
@@ -383,37 +403,55 @@ export const Navbar: React.FC = () => {
 }
 
 const NavigationSectionContent: React.FC<{
-  groups: RouteGroup[];
-  parentTitle: string;
+  groups: RouteGroup[]
+  parentTitle: string
 }> = ({ groups }) => {
-  const hasMultipleGroups = groups.length > 1;
+  const hasMultipleGroups = groups.length > 1
 
   return (
     <NavigationMenu.Content className="absolute top-full z-50 w-max rounded-md shadow-md bg-white hidden lg:block">
-      <div className={`p-2 sm:p-4 md:p-6 flex flex-col xl:flex-row ${hasMultipleGroups ? 'space-x-8' : ''} max-h-[80vh] overflow-y-auto`}>
+      <div
+        className={`p-2 sm:p-4 md:p-6 flex flex-col xl:flex-row ${hasMultipleGroups ? "space-x-8" : ""} max-h-[80vh] overflow-y-auto`}
+      >
         {groups.map((group, index) => (
           <div
             key={group.name}
-            className={`${hasMultipleGroups && index > 0 ? 'border-t border-l-0 xl:border-l xl:border-t-0 border-neutral-700 py-1 xl:py-0 xl:pl-6' : ''} ${hasMultipleGroups && index < 1 ? 'py-1 px-6 xl:py-0 xl:px-0' : ''} ${hasMultipleGroups ? 'flex-1' : ''}`}
+            className={`${hasMultipleGroups && index > 0 ? "border-t border-l-0 xl:border-l xl:border-t-0 border-neutral-700 py-1 xl:py-0 xl:pl-6" : ""} ${hasMultipleGroups && index < 1 ? "py-1 px-6 xl:py-0 xl:px-0" : ""} ${hasMultipleGroups ? "flex-1" : ""}`}
           >
-            {group.name && <h3 className="text-xl mb-2 sm:mb-3 md:mb-4 pl-2 tracking-widest max-w-[425px]">{group.name.toUpperCase()}</h3>}
+            {group.name && (
+              <h3 className="text-xl mb-2 sm:mb-3 md:mb-4 pl-2 tracking-widest max-w-[425px]">
+                {group.name.toUpperCase()}
+              </h3>
+            )}
             <ul className="list-none flex flex-col gap-1 sm:gap-1.5 md:gap-2">
               {group.routes.map((route) => (
-                <li key={route.href} className="hover:bg-neutral-50 focus:bg-neutral-100 p-1 sm:p-1.5 md:p-2">
+                <li
+                  key={route.href}
+                  className="hover:bg-neutral-50 focus:bg-neutral-100 p-1 sm:p-1.5 md:p-2"
+                >
                   <NavigationMenu.Link
                     href={route.href}
-                    {...(route.href === "https://publications.ccv.brown.edu" ? { target: "_blank", rel: "noopener noreferrer" } : {})}
+                    {...(route.href === "https://publications.ccv.brown.edu"
+                      ? {
+                          target: "_blank",
+                          rel: "noopener noreferrer",
+                        }
+                      : {})}
                     className="flex items-start focus:outline-none"
                   >
                     {route.icon && (
                       <div className="mr-1 sm:mr-1.5 md:mr-2 h-[2rem] sm:h-[3rem] md:h-[4rem] w-[2rem] sm:w-[3rem] md:w-[4rem] bg-neutral-100/75 text-white rounded-md flex items-center justify-center">
-                        <route.icon className="h-1/2 w-1/2"/>
+                        <route.icon className="h-1/2 w-1/2" />
                       </div>
                     )}
                     <div className="flex flex-col">
-                      <span className="text-lg sm:text-xl font-semibold pb-0.5 sm:pb-0.75 md:pb-1">{route.name}</span>
+                      <span className="text-lg sm:text-xl font-semibold pb-0.5 sm:pb-0.75 md:pb-1">
+                        {route.name}
+                      </span>
                       {route.description && (
-                        <span className="max-w-[425px] text-sm sm:text-md text-neutral-500 italic mb-1 sm:mb-1.5 md:mb-3">{route.description}</span>
+                        <span className="max-w-[425px] text-sm sm:text-md text-neutral-500 italic mb-1 sm:mb-1.5 md:mb-3">
+                          {route.description}
+                        </span>
                       )}
                     </div>
                   </NavigationMenu.Link>
@@ -424,8 +462,8 @@ const NavigationSectionContent: React.FC<{
         ))}
       </div>
     </NavigationMenu.Content>
-  );
-};
+  )
+}
 
 export default Navbar
 export { routes }
