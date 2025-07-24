c7286d12509bdf2226f4a2f7123d2d3c472c64e8
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-07-24 13:43:42 -0400

    chore: about pages pass pa11y

--- app/about/contact/page.tsx
+++ app/about/contact/page.tsx
@@ -1,25 +1,37 @@
-import React from "react";
-import Markdown from 'react-markdown';
-import rehypeRaw from 'rehype-raw'
-import remarkGfm from 'remark-gfm'
-import { Hero } from "@/components/Hero";
-import { SectionHeader } from "@/components/ui/section-header";
-import { Button } from "@/components/ui/button";
-import { Card, CardContent, CardHeader, CardTitle, CardDescription } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { cardVariants } from "@/components/ui/variants";
+import React from "react"
+import Markdown from "react-markdown"
+import rehypeRaw from "rehype-raw"
+import remarkGfm from "remark-gfm"
+import { Hero } from "@/components/Hero"
+import { SectionHeader } from "@/components/ui/section-header"
+import { Button } from "@/components/ui/button"
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+  CardDescription,
+} from "@/components/ui/card"
+import { cn } from "@/lib/utils"
+import { cardVariants } from "@/components/ui/variants"
 import { readContentFile } from "@/lib/content-utils"
-import Icon from "@/components/ui/render-icon";
-import { ContactUsTypes, OfficeHoursTypes, PageContentData } from "@/lib/about-types";
-import ExternalLink from "@/components/ui/external-link";
+import Icon from "@/components/ui/render-icon"
+import {
+  ContactUsTypes,
+  OfficeHoursTypes,
+  PageContentData,
+} from "@/lib/about-types"
+import ExternalLink from "@/components/ui/external-link"
 
-  const loadedContent = await readContentFile<PageContentData>('content/about/contact.yaml');
-const pageContent: PageContentData = loadedContent.data;
+const loadedContent = await readContentFile<PageContentData>(
+  "content/about/contact.yaml"
+)
+const pageContent: PageContentData = loadedContent.data
 
 export default async function ContactUs() {
   return (
     <div>
-      <Hero 
+      <Hero
         image={"/images/hero/about-kayaks.png"}
         title="Contact Us"
         description={pageContent.description}
@@ -39,12 +51,15 @@ export default async function ContactUs() {
         <div className="content-wrapper flex justify-center px-40">
           <div className="flex flex-wrap justify-center gap-y-6 gap-x-6 xs:w-1/2">
             {pageContent?.contactUs?.cards?.map((card: ContactUsTypes) => (
-              <div
-                key={card.title}
-                className="flex-grow max-w-lg"
-              >
+              <div key={card.title} className="flex-grow max-w-lg">
                 <div className="inline-flex items-center gap-2 w-full h-full">
-                  <Card className={cn("overflow-hidden flex flex-col w-full", cardVariants({ variant: "default" }), "h-full")}>
+                  <Card
+                    className={cn(
+                      "overflow-hidden flex flex-col w-full",
+                      cardVariants({ variant: "default" }),
+                      "h-full"
+                    )}
+                  >
                     <CardContent className="flex flex-col h-full px-6 mx-2">
                       <div className="relative border-b border-neutral-300">
                         <CardHeader className="flex flex-row gap-4 items-center">
@@ -52,17 +67,22 @@ export default async function ContactUs() {
                           {card.title}
                         </CardHeader>
                       </div>
-                      <CardDescription className="text-lg px-6 flex-grow pt-4">{card.description}</CardDescription>
+                      <CardDescription className="text-lg px-6 flex-grow pt-4">
+                        {card.description}
+                      </CardDescription>
                       <div className="px-6 flex flex-col gap-2">
-                        {card.buttonLinks && card.buttonLinks.map((link, index) => (
-                          <Button
-                            key={index}
-                            variant="primary_filled"
-                            size="xl"
-                          >
-                            <ExternalLink href={link.href} external={true}>{link.text}</ExternalLink>
-                          </Button>
-                        ))}
+                        {card.buttonLinks &&
+                          card.buttonLinks.map((link, index) => (
+                            <Button
+                              key={index}
+                              variant="primary_filled"
+                              size="xl"
+                            >
+                              <ExternalLink href={link.href} external={true}>
+                                {link.text}
+                              </ExternalLink>
+                            </Button>
+                          ))}
                       </div>
                     </CardContent>
                   </Card>
@@ -86,33 +106,44 @@ export default async function ContactUs() {
         <section className="content-wrapper">
           <div className="flex flex-wrap justify-center gap-y-6 gap-x-6">
             {pageContent?.officeHours?.cards?.map((card: OfficeHoursTypes) => (
-              <div
-                key={card.title}
-                className="flex-grow max-w-md"
-              >
+              <div key={card.title} className="flex-grow max-w-md">
                 <div className="inline-flex items-center gap-2 py-8 w-full h-full">
-                  <Card className={cn("overflow-hidden flex flex-col w-full h-full", cardVariants({ variant: "default" }))}>
+                  <Card
+                    className={cn(
+                      "overflow-hidden flex flex-col w-full h-full",
+                      cardVariants({ variant: "default" })
+                    )}
+                  >
                     <CardContent className="flex flex-col h-full">
                       <div className="relative border-b border-neutral-900 px-4 flex justify-center">
-                        <CardHeader className="text-center">{card.title}</CardHeader>
+                        <CardHeader className="text-center">
+                          {card.title}
+                        </CardHeader>
                       </div>
-                      <CardDescription className="pt-6 text-xl text-center">{card.subtitle}</CardDescription>
+                      <CardDescription className="pt-6 text-xl text-center">
+                        {card.subtitle}
+                      </CardDescription>
                       <CardTitle className="text-lg text-center flex-grow">
-                        <Markdown 
-                          rehypePlugins={[rehypeRaw]} 
+                        <Markdown
+                          rehypePlugins={[rehypeRaw]}
                           remarkPlugins={[remarkGfm]}
                         >
                           {card.description}
                         </Markdown>
                       </CardTitle>
                       <div className="flex justify-center mt-auto">
-                        {card.buttonLinks && card.buttonLinks.map((link, index) => (
-                          <Button key={index} variant="primary_filled" size="xl">
-                            <ExternalLink href={link.href}>
-                              {link.text}
-                            </ExternalLink>
-                          </Button>
-                        ))}
+                        {card.buttonLinks &&
+                          card.buttonLinks.map((link, index) => (
+                            <Button
+                              key={index}
+                              variant="primary_filled"
+                              size="xl"
+                            >
+                              <ExternalLink href={link.href}>
+                                {link.text}
+                              </ExternalLink>
+                            </Button>
+                          ))}
                       </div>
                     </CardContent>
                   </Card>
@@ -138,6 +169,7 @@ export default async function ContactUs() {
           </Card>
           <div className="w-full xl:w-1/2">
             <iframe
+              title="Map of 180 George St"
               className="
                 w-full
                 -mt-2 xl:-my-24 block
@@ -155,5 +187,5 @@ export default async function ContactUs() {
         </div>
       </section>
     </div>
-  );
+  )
 }
