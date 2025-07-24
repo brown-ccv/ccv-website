f5e8ac895a8ae446b650a934efe5d73366437888
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-07-24 10:39:04 -0400

    fix: use Next Link

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,15 +2,14 @@ import ExternalLink from "@/components/ui/external-link"
 import { Button } from "@/components/ui/button"
 import React from "react"
 import Hero from "@/components/Hero"
+import Link from "next/link"
 
 export default function NotFound() {
   return (
     <div>
       <Hero title="Oops!" description="It looks like this page does not exist">
         <Button variant="primary_filled" size="xl">
-          <ExternalLink href="/" external={false}>
-            Return Home
-          </ExternalLink>
+          <Link href="/">Return Home</Link>
         </Button>
       </Hero>
     </div>
