a2a5f5b95463e05ed7f2b982a1e7d2a029f7ede2
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-08-01 14:51:01 -0400

    refactor: ContentSection and SectionHeader to components
    let's reserve the ui folder for radix

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from "react"
 import { cn } from "@/lib/utils"
-import { SectionHeader } from "@/components/ui/SectionHeader"
+import { SectionHeader } from "@/components/SectionHeader"
 
 interface ContentSectionProps {
   children: ReactNode
