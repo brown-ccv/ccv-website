ab53967368afab1cc5f32c2db5e5c26e02954cd8
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-08-04 11:05:36 -0400

    chore: remove unused contentsection

--- app/services/rates/page.tsx
+++ app/services/rates/page.tsx
@@ -1,5 +1,4 @@
 import { Hero } from "@/components/Hero"
-import { ContentSection } from "@/components/ContentSection"
 import RatesContent from "@/content/services/rates.mdx"
 import { getMDXMetadata } from "@/lib/mdx-utils"
 
