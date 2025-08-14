5c2f574242ac6becf4e2bbd4beb0b23654351ced
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-08-14 15:12:49 -0400

    feat: ProjectEstimationSection

--- components/ProjectEstimateTab.tsx
+++ components/ProjectEstimateTab.tsx
@@ -9,6 +9,7 @@ interface ProjectEstimateTabProps {
   group: string
   time: string
   link?: string
+  goal: string
   image: string
   alt: string
 }
@@ -19,32 +20,44 @@ export const ProjectEstimateTab: React.FC<ProjectEstimateTabProps> = ({
   group,
   time,
   link,
+  goal,
   image,
   alt,
 }) => {
   return (
-    <div className="flex justify-between">
-      {/*Project Section*/}
-      <div>
-        <h3>{title}</h3>
-        <div className="flex items-center">
-          <Icon iconName="FaUser" className="w-6 h-6" />
-          <p className="text-lg lg:text-xl leading-snug font-semibold">
-            {group}
-          </p>
+    <div className="flex flex-col">
+      <div className="flex flex-col items-center justify-between lg:flex-row  gap-4">
+        {/*Project Section*/}
+        <div>
+          <h3>{title}</h3>
+          <div className="flex items-center gap-4">
+            <Icon iconName="FaUser" className="w-6 h-6" />
+            <p className="text-lg lg:text-xl leading-snug font-semibold">
+              {group}
+            </p>
+          </div>
+          {/* Time to Complete */}
+          <div className="flex items-center gap-4">
+            <Icon iconName="FaClock" className="w-6 h-6 text-keppel-700" />
+            <p className="text-lg lg:text-lg font-semibold">{time}</p>
+          </div>
+          <p>{description}</p>
+          {link && (
+            <ButtonLink href={link} external={true} title={title} size="md" />
+          )}
         </div>
-        {/* Time to Complete */}
-        <div className="flex items-center">
-          <Icon iconName="FaClock" className="w-6 h-6 text-keppel-700" />
-          <p className="text-lg lg:text-lg font-semibold">{time}</p>
+        {/*Image*/}
+        <div className="relative w-full flex justify-end">
+          <Image
+            height={600}
+            width={600}
+            src={image}
+            alt={alt}
+            className="object-center"
+          />
         </div>
-        <p>{description}</p>
-        {link && (
-          <ButtonLink href={link} external={true} title={title} size="md" />
-        )}
       </div>
-      {/*Image*/}
-      <Image src={image} alt={alt} />
+      <p>{goal}</p>
     </div>
   )
 }
