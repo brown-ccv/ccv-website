122ea76ec7b9eaec60849d9b8d0dfddb5e13a038
ashley <ashley_lee@brown.edu>
2025-08-15 10:07:46 -0400

    feat: add test portfolio data and interface

---
+++ lib/portfolio-types.ts
@@ -0,0 +1,28 @@
+export interface Person {
+    name: string;
+    github_user?: string;
+}
+
+export interface Link {
+    category: string;
+    url: string;
+}
+
+export interface Investigator {
+    name: string;
+    link: string;
+}
+
+export interface PortfolioEntry {
+    title: string;
+    description: string;
+    'project-type': string;
+    starred: boolean;
+    people: Person[];
+    languages?: string[];
+    tags?: string[];
+    groups?: string[];
+    department?: string[];
+    links?: Link[];
+    investigators?: Investigator[];
+}
