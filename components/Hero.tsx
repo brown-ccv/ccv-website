f6727dd0c8b9b7620c3dec20467a919b08e35f6e
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-08-29 16:55:35 -0400

    feat: mainhero vs every other hero

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -19,6 +19,50 @@ export const Hero = ({
   description,
   children,
   showGradient = true,
+  titleClassName = "xl:text-5xl",
+}: HeroProps) => {
+  return (
+    <div
+      className="relative flex w-full flex-col overflow-hidden bg-cover bg-[center_65%] p-12 text-white lg:px-14 lg:py-32"
+      style={{
+        backgroundImage: `
+          linear-gradient(135deg, rgb(17 24 39) 0%, rgb(243 244 246) 100%),
+          radial-gradient(circle at 0% 0%, #060839 0%, transparent 85%),
+          radial-gradient(circle at 100% 100%, #060839 0%, transparent 85%),
+          radial-gradient(circle at 70% 30%, #EC4899 0%, #8B5CF6 50%, transparent 70%),
+          radial-gradient(circle at 30% 70%, #00b398 0%, transparent 60%),
+          radial-gradient(ellipse at 50% 50%, #04c8a6 0%, transparent 40%),
+          linear-gradient(135deg, #00b398 0%, transparent 30%, transparent 70%, #EC4899 100%),
+          url(${image})
+        `,
+        backgroundSize: "cover",
+        backgroundBlendMode:
+          "overlay, multiply, multiply, overlay, soft-light, overlay, overlay, normal",
+      }}
+    >
+      {showGradient ? (
+        <div className="flex w-full max-w-[1400px] flex-col gap-9">
+          {title && <h1 className={titleClassName}>{title}</h1>}
+          {description && <p className="text-xl md:text-2xl">{description}</p>}
+          {children && (
+            <div className="flex flex-col flex-wrap gap-2 pt-8 sm:flex-row sm:gap-4">
+              {children}
+            </div>
+          )}
+        </div>
+      ) : (
+        children
+      )}
+    </div>
+  )
+}
+
+export const MainHero = ({
+  image,
+  title,
+  description,
+  children,
+  showGradient = true,
   titleClassName = "xl:text-6xl",
   descriptionClassName = "text-xl md:text-2xl xl:text-3xl",
 }: HeroProps) => {
