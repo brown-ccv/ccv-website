7232f97348d8ecd82b3f22d30a3c5ef42223f76c
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-08-29 09:24:41 -0400

    chore: remove matching services for cards

--- components/storage/StorageCards.tsx
+++ components/storage/StorageCards.tsx
@@ -20,9 +20,6 @@ const StorageCards: React.FC<TableProps> = ({
 }) => {
   return (
     <div className="flex flex-col items-center lg:items-start">
-      <p className="mb-4 text-sm text-gray-500">
-        Showing {} matching service(s)
-      </p>
       <div className="flex flex-wrap justify-center gap-4 lg:justify-start">
         {services.table_data.map((service, index) => (
           <StorageServiceCard
