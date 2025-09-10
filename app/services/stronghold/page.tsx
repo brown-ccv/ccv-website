f2494078381e6f0f1c0d4fccf1b01913466da194
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-09-10 15:48:25 -0400

    chore: update/reorganize stronghold page

--- app/services/stronghold/page.tsx
+++ app/services/stronghold/page.tsx
@@ -1,13 +1,31 @@
 import { Hero } from "@/components/Hero"
 import StrongholdContent from "@/content/services/stronghold.mdx"
 import { getMDXMetadata } from "@/lib/mdx-utils"
+import ButtonLink from "@/components/button/ButtonLink"
 
 export default function Stronghold() {
   const metadata = getMDXMetadata("content/services/stronghold.mdx")
 
   return (
     <>
-      <Hero title={metadata.title} description={metadata.description} />
+      <Hero title={metadata.title} description={metadata.description}>
+        <ButtonLink
+          variant="primary_filled"
+          size="lg"
+          href="https://brown.atlassian.net/servicedesk/customer/portal/22/group/62/create/256"
+          external={true}
+        >
+          Request Environment
+        </ButtonLink>
+        <ButtonLink
+          variant="secondary_filled"
+          size="lg"
+          href="https://docs.ccv.brown.edu/stronghold/"
+          external={true}
+        >
+          Documentation
+        </ButtonLink>
+      </Hero>
       <StrongholdContent />
     </>
   )
