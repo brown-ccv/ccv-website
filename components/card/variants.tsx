8bdbbc6d996033a3c35ca688e49424733605d4c8
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-09-16 09:57:11 -0400

    fix: rates cards size medium

--- components/card/variants.tsx
+++ components/card/variants.tsx
@@ -5,7 +5,7 @@ export const CardVariants = cva("bg-white", {
     size: {
       custom: "",
       sm: "max-w-sm",
-      md: "max-w-md",
+      md: "max-w-md w-80 flex-shrink-0",
       lg: "max-w-lg flex-shrink-0",
     },
   },
