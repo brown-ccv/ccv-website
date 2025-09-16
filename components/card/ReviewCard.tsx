a21d47241722cb0845438af531dc172fefe4aba0
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-09-16 13:22:32 -0400

    feat: Hover shows review

--- components/card/ReviewCard.tsx
+++ components/card/ReviewCard.tsx
@@ -2,6 +2,12 @@
 import { cn } from "@/lib/utils"
 import React, { useState } from "react"
 import Image from "next/image"
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+  HoverCardPortal,
+} from "@/components/ui/HoverCard"
 
 interface ReviewCardProps {
   img?: string
@@ -10,11 +16,16 @@ interface ReviewCardProps {
   review?: string
 }
 
-const ReviewCard: React.FC<ReviewCardProps> = ({
+interface CollabCardProps extends ReviewCardProps {
+  className?: string
+}
+
+const CollabCard: React.FC<CollabCardProps> = ({
   img,
   name,
   department,
   review,
+  className = "",
 }) => {
   const [imgSrc, setImgSrc] = useState(img)
 
@@ -24,8 +35,9 @@ const ReviewCard: React.FC<ReviewCardProps> = ({
   return (
     <figure
       className={cn(
-        "relative h-full w-[420px] cursor-pointer overflow-hidden rounded-xl border p-4",
-        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]"
+        "relative h-full w-[420px] overflow-hidden rounded-xl border p-4",
+        "border-gray-950/[.1] bg-gray-950/[.01] hover:bg-gray-950/[.05]",
+        className
       )}
     >
       <div className="flex flex-row items-center gap-2">
@@ -42,17 +54,41 @@ const ReviewCard: React.FC<ReviewCardProps> = ({
           <p className="text-xs font-medium">{department}</p>
         </div>
       </div>
-      {review &&
-        (() => {
-          const words = review.trim().split(/\s+/)
-          const truncatedReview =
-            words.slice(0, 20).join(" ") + (words.length > 12 ? "..." : "")
-          return (
-            <blockquote className="mt-2 text-sm">{truncatedReview}</blockquote>
-          )
-        })()}
+      {review && <blockquote className="mt-2 text-sm">{review}</blockquote>}
     </figure>
   )
+}
+
+const ReviewCard: React.FC<ReviewCardProps> = ({
+  img,
+  name,
+  department,
+  review,
+}) => {
+  if (review) {
+    const words = review.trim().split(/\s+/)
+    const truncatedReview =
+      words.slice(0, 20).join(" ") + (words.length > 12 ? "..." : "")
+    return (
+      <HoverCard>
+        <HoverCardTrigger>
+          <CollabCard
+            img={img}
+            name={name}
+            department={department}
+            review={truncatedReview}
+            className="cursor-pointer"
+          />
+        </HoverCardTrigger>
+        <HoverCardPortal>
+          <HoverCardContent className="bg-white">
+            <blockquote className="mt-2 text-sm">{review}</blockquote>
+          </HoverCardContent>
+        </HoverCardPortal>
+      </HoverCard>
+    )
+  }
+  return <CollabCard img={img} name={name} department={department} />
 }
 
 export default ReviewCard
