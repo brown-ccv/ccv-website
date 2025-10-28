a0925e92ca29d6ee268104b670ea59729b4033ab
George Dang <53052793+gtdang@users.noreply.github.com>
2025-10-28 10:54:45 -0400

    refactor: simplify GET function by removing unnecessary type definitions

--- app/api/workday/route.ts
+++ app/api/workday/route.ts
@@ -1,10 +1,6 @@
 import { getWorkdayData } from "@/app/about/queries"
 
-interface WorkdayRequest extends Request {}
-
-type WorkdayResponse = Response
-
-export async function GET(req: WorkdayRequest): Promise<WorkdayResponse> {
+export async function GET(req: Request): Promise<Response> {
   const data = await getWorkdayData()
   return new Response(JSON.stringify(data), {
     headers: { "Content-Type": "application/json" },
