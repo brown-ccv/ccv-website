a91b0cdc3755f42262738ac22d944792ff519f41
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-11-18 11:21:17 -0500

    style: ContentSection components

--- components/ContentSection.tsx
+++ components/ContentSection.tsx
@@ -1,44 +1,80 @@
 import React, { ReactNode } from "react"
 import { cn } from "@/lib/utils"
 import { SectionHeader } from "@/components/SectionHeader"
+import CCVBars from "@/components/assets/CCVBars"
+
+interface ContentTitleProps {
+  title: string
+  bars?: boolean
+  icon?: React.ReactNode
+}
+
+const ContentTitle = React.forwardRef<
+  HTMLDivElement,
+  ContentTitleProps & React.HTMLAttributes<HTMLDivElement>
+>(({ bars = true, title, icon, className, ...props }, ref) => (
+  <>
+    {bars && <CCVBars />}
+    <h2
+      ref={ref} // Make sure to forward the ref
+      className={cn("pb-2 tracking-tighter", icon ? "flex" : "", className)}
+      aria-label={title}
+      {...props}
+    >
+      {icon && (
+        <span className="mr-3" aria-hidden="true">
+          {icon}
+        </span>
+      )}
+      {title}
+    </h2>
+  </>
+))
+ContentTitle.displayName = "ContentTitle"
+
+const ContentHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div
+    className={cn(
+      `getHeaderAlignFromParent flex flex-col pb-6`, // NOTE: this conditionally applies left align for header; see globals.css
+      className
+    )}
+    {...props}
+  />
+))
+ContentHeader.displayName = "ContentHeader"
+
+const ContentSubHeader = React.forwardRef<
+  HTMLDivElement,
+  React.HTMLAttributes<HTMLDivElement>
+>(({ className, ...props }, ref) => (
+  <div className={cn("space-y-6 pt-4", className)} {...props} />
+))
+ContentSubHeader.displayName = "ContentSubHeader"
 
 interface ContentSectionProps {
-  children: ReactNode
-  title: string
-  id?: string
-  icon?: React.ReactNode
-  subHeader?: React.ReactNode
   align?: "left" | "center"
-  className?: string
 }
 
-export const ContentSection = ({
-  children,
-  title,
-  id,
-  icon,
-  subHeader,
-  align = "center",
-  className,
-}: ContentSectionProps) => {
-  return (
-    <section
-      id={id}
-      className={cn(
-        "px-page w-full space-y-4 py-12 even:bg-neutral-50",
-        align === "left"
-          ? "flex flex-col gap-24 lg:flex-row lg:justify-between lg:space-y-0"
-          : "",
-        className
-      )}
-    >
-      <SectionHeader
-        title={title}
-        align={align}
-        icon={icon}
-        subHeader={subHeader}
-      />
-      {children}
-    </section>
-  )
-}
+const ContentSection = React.forwardRef<
+  HTMLDivElement,
+  ContentSectionProps & React.HTMLAttributes<HTMLDivElement>
+>(({ align = "center", className, ...props }, ref) => (
+  <section
+    className={cn(
+      "w-full space-y-4 px-12 py-12 even:bg-neutral-50 sm:px-16 lg:px-14 xl:px-20",
+      align === "left"
+        ? "flex flex-col lg:flex-row lg:justify-between lg:gap-24 lg:space-y-0"
+        : "",
+      className
+    )}
+    data-align={align}
+    {...props}
+  />
+))
+
+ContentSection.displayName = "ContentSection"
+
+export { ContentSection, ContentTitle, ContentSubHeader, ContentHeader }
