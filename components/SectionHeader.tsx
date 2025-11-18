d3ca6c36e1c4dd9ac0adfced5a22a8dfd1c84983
Heather Yu <35639529+hetd54@users.noreply.github.com>
2025-11-18 12:53:51 -0500

    style: padding in HeroCard

--- components/SectionHeader.tsx
+++ components/SectionHeader.tsx
@@ -24,7 +24,7 @@ export const SectionHeader: React.FC<SectionHeaderProps> = ({
   return (
     <div
       className={cn(
-        `not-prose flex flex-col pb-6 ${align === "center" ? "items-center" : "max-w-1/2 items-center lg:items-start"}`,
+        `not-prose flex flex-col ${align === "center" ? "items-center" : "max-w-1/2 items-center lg:items-start"}`,
         className
       )}
     >
