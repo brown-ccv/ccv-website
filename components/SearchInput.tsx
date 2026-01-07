9955fc86adc8f3fbaeb495f2e543aa5581048954
Heather Yu <35639529+hetd54@users.noreply.github.com>
2026-01-07 16:45:06 -0500

    style: hide search clear button

--- components/SearchInput.tsx
+++ components/SearchInput.tsx
@@ -4,11 +4,13 @@ import React, { useState } from "react"
 
 interface SearchInputProps {
   placeholder?: string
+  name: string
   onSearchChange: (value: string) => void
 }
 
 const SearchInput: React.FC<SearchInputProps> = ({
   placeholder = "Search...",
+  name,
   onSearchChange,
 }) => {
   const [query, setQuery] = useState("")
@@ -20,16 +22,17 @@ const SearchInput: React.FC<SearchInputProps> = ({
   }
 
   return (
-    <div className="relative w-full max-w-sm">
+    <div className="relative">
       <div className="pointer-events-none absolute inset-y-0 left-0 flex items-center pl-3">
-        <Icon iconName="FaSearch" className="h-5 w-5" />
+        <Icon iconName="FaSearch" className="h-4 w-4" />
       </div>
       <Input
         type="search"
+        name={name}
         value={query}
         onChange={handleChange}
         placeholder={placeholder}
-        className="block w-full rounded-lg border border-gray-300 bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus:border-sunglow-500 focus:ring-sunglow-500"
+        className="block w-full rounded-lg border-none bg-gray-50 p-2.5 pl-10 text-sm text-gray-900 focus-visible:ring-0 [&::-ms-clear]:hidden [&::-webkit-search-cancel-button]:hidden"
       />
     </div>
   )
