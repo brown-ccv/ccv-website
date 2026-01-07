9661d7105cede98448bd51ef368fb7fa860a0e3e
Heather Yu <35639529+hetd54@users.noreply.github.com>
2026-01-07 16:46:14 -0500

    refactor: use shadcn dialog

--- components/SiteSearch.tsx
+++ components/SiteSearch.tsx
@@ -1,31 +1,38 @@
 import SearchInput from "@/components/SearchInput"
 import React, { useState } from "react"
 import {
-  StyledDialog,
-  StyledDialogContent,
-  StyledDialogTitle,
-  StyledDialogTrigger,
-} from "@/components/StyledDialog"
+  Dialog,
+  DialogClose,
+  DialogContent,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/Dialog"
 import { Button } from "@/components/button/Button"
 import { FaMagnifyingGlass } from "react-icons/fa6"
+import { IconButton } from "@/components/button/IconButton"
+import { Input } from "@/components/ui/Input"
 
 const SiteSearch: React.FC = ({}) => {
   const [searchQuery, setSearchQuery] = useState<string>("")
   return (
-    <StyledDialog onOpenChange={() => setSearchQuery("")}>
-      <StyledDialogTrigger asChild>
+    <Dialog onOpenChange={() => setSearchQuery("")}>
+      <DialogTrigger asChild>
         <Button
           variant="icon_only"
           iconOnly={<FaMagnifyingGlass />}
           className="bg-red-university text-white"
         />
-      </StyledDialogTrigger>
-      <StyledDialogContent className="bg-white">
-        <StyledDialogTitle>Search CCV</StyledDialogTitle>
-        <SearchInput onSearchChange={(val) => setSearchQuery(val)} />
+      </DialogTrigger>
+      <DialogContent className="bg-white p-1">
+        <DialogTitle className="border-b border-gray-400">
+          <SearchInput
+            name="site-search"
+            onSearchChange={(val) => setSearchQuery(val)}
+          />
+        </DialogTitle>
         <p>{searchQuery}</p>
-      </StyledDialogContent>
-    </StyledDialog>
+      </DialogContent>
+    </Dialog>
   )
 }
 
