3c95c233cf8f90dde43fcb4996c59412ee93e7ca
Heather Yu <35639529+hetd54@users.noreply.github.com>
2026-03-11 15:54:44 -0400

    feat: use DialogClose to close from link

--- components/Search.tsx
+++ components/Search.tsx
@@ -10,7 +10,7 @@ import {
 } from "react-instantsearch"
 import type { Hit as AlgoliaHit } from "instantsearch.js"
 import { Link } from "./Link"
-import { X } from "lucide-react"
+import { DialogClose } from "@/components/ui/Dialog"
 import * as React from "react"
 import { urlToBreadcrumb } from "@/lib/utils"
 
@@ -59,24 +59,26 @@ function SearchResults() {
 
 function Hit({ hit }: HitProps) {
   return (
-    <Link
-      href={hit.url}
-      className="focus-visible:ring-ring group flex items-start gap-3 rounded-md px-4 py-3 transition-colors hover:bg-slate-100 focus:outline-none focus-visible:bg-slate-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sunglow-400"
-    >
-      {/* Content */}
-      <div className="min-w-0 flex-1 space-y-1">
-        <p className="text-xs text-gray-500">{urlToBreadcrumb(hit.url)}</p>
-        <h3 className="mb-1 font-medium">
-          <Highlight attribute="title" hit={hit} />
-        </h3>
+    <DialogClose asChild>
+      <Link
+        href={hit.url}
+        className="focus-visible:ring-ring group flex items-start gap-3 rounded-md px-4 py-3 transition-colors hover:bg-slate-100 focus:outline-none focus-visible:bg-slate-100 focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-sunglow-400"
+      >
+        {/* Content */}
+        <div className="min-w-0 flex-1 space-y-1">
+          <p className="text-xs text-gray-500">{urlToBreadcrumb(hit.url)}</p>
+          <h3 className="mb-1 font-medium">
+            <Highlight attribute="title" hit={hit} />
+          </h3>
 
-        {hit.description && (
-          <p className="line-clamp-2 text-sm text-gray-700">
-            <Highlight attribute="description" hit={hit} />
-          </p>
-        )}
-      </div>
-    </Link>
+          {hit.description && (
+            <p className="line-clamp-2 text-sm text-gray-700">
+              <Highlight attribute="description" hit={hit} />
+            </p>
+          )}
+        </div>
+      </Link>
+    </DialogClose>
   )
 }
 
