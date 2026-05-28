ef1032e7b821f492b16a6d9d727888265550d077
s-bessey <sam_bessey@brown.edu>
2026-05-28 09:36:09 -0400

    feat: add pulses

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -2,6 +2,7 @@ import React from "react"
 import ActiveIssues from "@/components/status/ActiveIssues"
 import IssueGrid from "@/components/status/IssueGrid"
 import { getOpenIssues } from "@/lib/fetch-issues"
+import "./status.css"
 
 export default async function ActiveStatus() {
   const issues = await getOpenIssues()
