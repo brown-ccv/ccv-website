3078547bdb0ed97055575b8a71084695930e7e9f
s-bessey <sam_bessey@brown.edu>
2026-05-28 10:52:01 -0400

    fix: cache open issues

--- src/app/status/page.tsx
+++ src/app/status/page.tsx
@@ -4,8 +4,25 @@ import IssueGrid from "@/components/status/IssueGrid"
 import { getOpenIssues } from "@/lib/fetch-issues"
 import "./status.css"
 
+let getCachedOpenIssues: () => Promise<any[]>
+if (!process.env.NEXT_PUBLIC_STATIC_EXPORT) {
+  const { getOpenIssues } = require("@/lib/fetch-issues")
+  const { unstable_cache } = require("next/cache")
+  getCachedOpenIssues = unstable_cache(getOpenIssues, ["open-issues"], {
+    revalidate: 60,
+  })
+} else {
+  getCachedOpenIssues = async () => []
+}
+
 export default async function ActiveStatus() {
-  const issues = await getOpenIssues()
+  let issues = []
+  try {
+    issues = await getCachedOpenIssues()
+  } catch (error) {
+    console.error("Failed to fetch GitHub issues:", error)
+  }
+
   return (
     <>
       <IssueGrid issues={issues}></IssueGrid>
