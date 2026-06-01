d4b2bacf3d5cb29c0bf772f74c81361f7a6f7c69
s-bessey <sam_bessey@brown.edu>
2026-06-01 11:49:52 -0400

    refactor: style updates for grid

--- src/components/status/IssueGrid.tsx
+++ src/components/status/IssueGrid.tsx
@@ -19,41 +19,39 @@ export default async function IssueGrid({ issues }: { issues: GridProps[] }) {
       {Object.values(issues).map((repo) => (
         <li
           key={repo.name}
-          className="col-span-1 divide-y divide-gray-200 rounded-lg bg-white shadow"
+          className="col-span-1 min-w-full divide-y divide-gray-200 rounded-lg bg-white shadow"
         >
           <div className="flex h-4/6 w-full items-center justify-between p-6">
-            <div className="flex items-start space-x-3">
+            <div className="flex items-start space-x-3 truncate">
               <h2
-                className="inline-flex flex-shrink-0 truncate text-lg font-medium leading-8"
+                className="inline-flex truncate text-nowrap text-lg font-medium leading-8"
                 style={{ lineHeight: "1.25rem" }}
               >
                 {repo.name}
               </h2>
-              <a
-                href={
-                  repo.openIssues.length !== 0 ? `#${repo.name}` : undefined
-                }
+              <div
                 style={{
                   border: `2px solid ${priorityStatus(repo.openIssues)?.color}`,
                 }}
-                className={`${repo.openIssues.length === 0 ? "disabled" : "hover:text-gray-600"} inline-flex flex-shrink-0 items-center rounded-full px-1.5 py-0.5 text-xs text-black no-underline`}
+                className={`${repo.openIssues.length === 0 && "disabled"} inline-flex items-center rounded-full px-1.5 py-0.5 text-xs text-black no-underline`}
               >
-                {priorityStatus(repo.openIssues)?.name}
-              </a>
+                <p>{priorityStatus(repo.openIssues)?.name}</p>
+              </div>
             </div>
-            <p className="mt-1 truncate text-gray-500">
-              {priorityStatus(repo.openIssues)?.title}{" "}
-              {repo.openIssues.length > 1
-                ? `+${repo.openIssues.length - 1} more`
-                : ""}
-            </p>
-            <span
-              style={{
-                backgroundColor: priorityStatus(repo.openIssues)?.color,
-                color: priorityStatus(repo.openIssues)?.color,
-              }}
-              className="pulse h-4 w-4 flex-shrink-0 rounded-full"
-            />
+            <div className="flex content-center items-center gap-2">
+              <p className="text-gray-500">
+                {repo.openIssues.length > 1
+                  ? `+${repo.openIssues.length - 1} more`
+                  : ""}
+              </p>
+              <span
+                style={{
+                  backgroundColor: priorityStatus(repo.openIssues)?.color,
+                  color: priorityStatus(repo.openIssues)?.color,
+                }}
+                className="pulse h-4 w-4 flex-shrink-0 overflow-visible rounded-full"
+              />
+            </div>
           </div>
           <div className="flex h-2/6 divide-x divide-gray-200">
             <a
